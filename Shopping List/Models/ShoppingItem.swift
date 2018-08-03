1e6f4538148874c25e34deb16068aa970eb5438a
Conner Alegre <cga0@pm.me>
2018-08-03 09:06:40 -0600

    Add Model

---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Conner on 8/3/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    var image: Data
+    var name: String
+    var addedToList: Bool
+}
