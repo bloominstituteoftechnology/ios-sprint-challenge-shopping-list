f4b013b0fe1fb235ce22ede82789c419e72a61a8
Samantha Gatt <smfgatt@gmail.com>
2018-08-03 13:27:38 -0400

    Configures local notification helper

---
+++ Shopping List/LocalNotificationHelper.swift
@@ -0,0 +1,39 @@
+//
+//  LocalNotificationHelper.swift
+//  Shopping List
+//
+//  Created by Samantha Gatt on 8/3/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class LocalNotificationHelper {
+    
+    func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                completion(settings.authorizationStatus)
+            }
+        }
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if let error = error { NSLog("Error requesting authorization status for local notifications: \(error)") }
+            
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+    
+    
+    func sendNotification(name: String, address: String) {
+        let content = UNMutableNotificationContent()
+        content.title = "Delivery for \(name)!"
+        content.body = "Your delivery will be shipped to \(address)"
+    }
+}
