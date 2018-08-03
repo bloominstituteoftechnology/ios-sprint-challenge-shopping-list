69ce71bae491dffb6141045565122839e5e3d5c4
Jeremy Taylor <jtaylor@jctaylor.org>
2018-08-03 11:15:40 -0500

    Implement UserDefaults to check if this is the first run of the App

---
+++ Shopping List/Controllers/ShoppingListCollectionViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ShoppingListCollectionViewController.swift
+//  Shopping List
+//
+//  Created by Jeremy Taylor on 8/3/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ShoppingListCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Check if it is the first run of the app
+        checkUserDefaults()
+        
+    }
+
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShoppingListCell", for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    func checkUserDefaults() {
+        let userDefaults = UserDefaults.standard
+        guard userDefaults.bool(forKey: appHasBeenRun) else {
+            // First time running the app
+            print("First Run!")
+            
+            // Setup shopping list here then set userDefaults key!
+            userDefaults.set(true, forKey: appHasBeenRun)
+            return
+            
+        }
+        
+        print("We've run the app before")
+    }
+
+}
---
+++ Shopping List/Views/ShoppingListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Jeremy Taylor on 8/3/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewCell: UICollectionViewCell {
+    
+}
