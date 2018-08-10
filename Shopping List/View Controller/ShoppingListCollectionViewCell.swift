b23b222ebf31332720260622a09853340dd73f76
Iyin Raphael <iyinraphael@gmail.com>
2018-08-10 10:57:15 -0700

    added cell controller code

--- Shopping List/View Controller/ShoppingListCollectionViewCell.swift
+++ Shopping List/View Controller/ShoppingListCollectionViewCell.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
+protocol ShoppingListCellDelegate: class {
+    func toggleHasBeenAdded(for cell: ShoppingListCollectionViewCell)
+}
+
 class ShoppingListCollectionViewCell: UICollectionViewCell {
     
+    weak var delegate: ShoppingListCellDelegate?
+    
+    var shoppingItem: Item?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func isAddedButton(_ sender: Any) {
+        delegate?.toggleHasBeenAdded(for: self)
+        
+    }
+    
+    func updateView(){
+        guard let imageCell = shoppingItem else {return}
+        imageView.image = UIImage(data: imageCell.image)
+        label.text = imageCell.name
+        
+        
+    }
 }
