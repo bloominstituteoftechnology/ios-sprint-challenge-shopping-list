20c3176fc4db2770fe6fd09cabe00934e0ccb4a8
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-10 13:31:20 -0400

    Set up detailview

--- Shopping List/View Controllers/OrderingViewController.swift
+++ Shopping List/View Controllers/OrderingViewController.swift
@@ -13,23 +13,42 @@ class OrderingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendOrder(_ sender: Any) {
+        guard let name = nameTextField.text,
+            let address = addressTextField.text else { return }
+        
+        localNotificationHelper.requestAuthorization { (success) in
+            if success == true {
+                self.localNotificationHelper.scheduleDailyReminderNotification(name: name, address: address)
+            }
+        }
     }
-    */
-
+    
+    func updateViews() {
+        guard let shoppingItems = shoppingItemController?.shoppingItems else { return }
+        
+        var numberOfItems = 0
+        for item in shoppingItems {
+            if item.isAdded {
+                numberOfItems += 1
+            }
+        }
+        textLabel?.text = "You currently have (\(numberOfItems) item(s) in your shopping cart."
+    }
+    
+    let localNotificationHelper = LocalNotificationHelper()
+    var shoppingItemController: ShoppingItemController? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
 }
