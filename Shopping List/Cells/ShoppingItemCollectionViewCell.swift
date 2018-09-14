cbfe2501f189c42ef2013a86c3f9f350832f3309
Paul Yi <yipaulc@gmail.com>
2018-09-14 10:13:31 -0700

    had hard time with storyboard. created controllers and connected outlets

---
+++ Shopping List/Cells/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Paul Yi on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Shopping List/View Controllers/ShoppingListDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ShoppingListDetailViewController.swift
+//  Shopping List
+//
+//  Created by Paul Yi on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func sendOrder(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var currentItemsLabel: UILabel!
+    
+    @IBAction func nameTextField(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addressTextField(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
