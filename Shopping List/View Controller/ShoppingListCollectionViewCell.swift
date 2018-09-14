26d835ea6d771f21f837a7997a266d5015f21b2a
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-14 11:07:44 -0500

    Storyboard set up completed

---
+++ Shopping List/View Controller/ShoppingListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Ilgar Ilyasov on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewCell: UICollectionViewCell {
+    
+}
