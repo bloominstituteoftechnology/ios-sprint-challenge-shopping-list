7d74dc5add584e34da068e9a753862ebff714247
Paul Yi <yipaulc@gmail.com>
2018-09-14 10:34:52 -0700

    timed out, incomplete

--- Shopping List/Cells/ShoppingItemCollectionViewCell.swift
+++ Shopping List/Cells/ShoppingItemCollectionViewCell.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
+protocol ShoppingItemCollectionViewCellDelegate: class {
+    func itemToggled(on cell: ShoppingItemCollectionViewCell)
+}
+
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
+    
+    
+    
+    weak var delegate: ShoppingItemCollectionViewCellDelegate?
+    
+    @IBOutlet weak var addedLabel: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
 }
+
+
+
--- Shopping List/View Controllers/ShoppingListDetailViewController.swift
+++ Shopping List/View Controllers/ShoppingListDetailViewController.swift
@@ -18,25 +18,16 @@ class ShoppingListDetailViewController: UIViewController {
     
 
     @IBAction func sendOrder(_ sender: Any) {
+        
     }
     
     
+   
     @IBOutlet weak var currentItemsLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
     
-    @IBAction func nameTextField(_ sender: Any) {
-    }
-    
-    
-    @IBAction func addressTextField(_ sender: Any) {
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    var shoppingItemController: ShoppingItemController?
+    var localNotificationHelper: LocalNotificationHelper?
 }
+
