e0d512dc37cee45093742f52601cfab54ec25550
Scott Bennett <stt.btt@gmail.com>
2018-09-14 11:26:53 -0400

    Storyboards and files setup

---
+++ Shopping List/Model Controllers/ShoppingItemController.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Scott Bennett on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Shopping List/View Controllers/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Scott Bennett on 9/14/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
