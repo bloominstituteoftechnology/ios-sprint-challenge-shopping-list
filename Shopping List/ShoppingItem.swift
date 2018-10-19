37e97138069dbffdf7afd90a6f1095281e71be17
Sean Doyle <sdoylelambda@gmail.com>
2018-10-19 13:13:06 -0400

    working on timer

--- Shopping List/ShoppingItem.swift
+++ Shopping List/ShoppingItem.swift
@@ -3,24 +3,49 @@ import UIKit
 class ShoppingItem: UIView {
 
 
-
-
-
-required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    let _ = loadViewFromNib()
-}
-
-    func loadViewFromNib() -> UIView {
-        let bundle = Bundle.init(for: type(of: self))
-        let nib = UINib(nibName: "ShoppingItem", bundle: bundle)
-        let view = nib.instantiate(withOwner: self, options: nil) [0] as! UIView
-        view.frame = bounds
-        view.autoresizingMask = [UIViewAutoresizing.flexibleWidth , UIViewAutoresizing.flexibleHeight]
-            addSubview(view)
-            return view
+//    let reuseIdentifier = "cell"
+//    var images: [UIImage] = []
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+//        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+//
+//
+    
+    
+    
+//   // var view: UIView!
+//    override init(frame: CGRect)
+//    {
+//        super.init(frame: frame)
+//   //     setup()
+//    }
+//////
+////
+////
+////
+////
+////
+//  //super.init(coder: aDecoder)
+////
+////
+//required init?(coder aDecoder: NSCoder) {
+//    UINib(nibName: "ShoppingItem", bundle: nil).instantiate(withOwner: self, options: nil)
+////    super.init(coder: aDecoder)
+//    let _ = loadViewFromNib()
+//}
+//
+//    func loadViewFromNib() -> UIView {
+//        let bundle = Bundle.init(for: type(of: self))
+//        let nib = UINib(nibName: "ShoppingItem", bundle: bundle)
+//        let view = nib.instantiate(withOwner: self, options: nil) [0] as! UIView
+//        view.frame = bounds
+//        view.autoresizingMask = [UIViewAutoresizing.flexibleWidth , UIViewAutoresizing.flexibleHeight]
+//            addSubview(view)
+//            return view
     }
 
 
 
-}
+
--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -1,25 +1,61 @@
+//import UIKit
 //
-//  ViewController.swift
-//  Shopping List
+//class ShoppingItem: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 //
-//  Created by Spencer Curtis on 8/3/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
+//    let reuseIdentifier = "cell"
+//    var images: [UIImage] = []
 //
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+//        collectionView?.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+//
+//        for i in 1...12 {
+//            guard let image = UIImage(named: "Image\(i)") else { return }
+//            images.append(image)
+//        }
+//        collectionView?.allowsMultipleSelection = true
+//    }
+//
+//    let targetDimension: CGFloat = 320
+//
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        guard let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout else {fatalError("unable to retrieve layout")}
+//
+//        let insetAmount: CGFloat = 32
+//        layout.minimumLineSpacing = .greatestFiniteMagnitude
+//        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return images.count
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+////        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell else {fatalError("No Cell")}
+//
+////        cell.imageView.image = images[indexPath.row]
+//        return nib
+////    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath)  -> CGSize {
+//        let image = images[indexPath.row]
+//        let maxDimension = max(image.size.width, image.size.height)
+//        let scale = targetDimension / maxDimension
+//        return CGSize(width: image.size.width * scale, height: image.size.height * scale)
+//    }
+//
+////    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+////        guard let cell = collectionView.cellForItem(at: indexPath) else {return}
+////        cell.layer.borderWidth = 1
+////        cell.layer.borderColor = UIColor.green.cgColor
+////    }
+////    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+////        guard let cell = collectionView.cellForItem(at: indexPath) else {return}
+////        cell.layer.borderWidth = 1
+////        cell.layer.borderColor = UIColor.black.cgColor
+//    }
+//}
+//
