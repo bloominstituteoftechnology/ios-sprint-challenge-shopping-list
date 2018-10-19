2116ad310dc53040ad6dd6ac4a57cbe1f429d0e3
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-19 10:16:14 -0700

    added needed model/controller

--- Shopping List/OrderShoppingListItemsViewController.swift
+++ Shopping List/OrderShoppingListItemsViewController.swift
@@ -4,8 +4,19 @@ class OrderShoppingListItemsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    @IBOutlet weak var itemCountLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBAction func orderButton(_ sender: Any) {
+        
+        //authorizer permision for notifications
+        
+    }
+    
+    
     
 
  
--- Shopping List/ShoppingItem.swift
+++ Shopping List/ShoppingItem.swift
@@ -1,5 +1,11 @@
 import UIKit
 
-let itemNames = ["apple", "grapes", "milk", "muffin", "popcorn", "soda", "strawberries"]
-
-
+class ShoppingItem: Codable {
+    
+    let name: String
+    var isAdded: Bool
+    
+    init(name: String, isAdded: Bool = false) {
+        (self.name, self.isAdded) = (name, isAdded)
+    }
+}
