bb230bbab77893360fbebd7ee27eadf15facf3ab
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-19 09:05:38 -0700

    updated story board and create function

--- Shopping List/Model + Controller/ShoppingItemController.swift
+++ Shopping List/Model + Controller/ShoppingItemController.swift
@@ -10,7 +10,24 @@ import Foundation
 import UIKit
 
 class ShoppingItemContorller {
+
+    // create array
+    // persistence
+    // create func
+    // load items func
+    // user defaults
+
+    private(set) var shoppingItems: [ShoppingItem] = []
     
+    func createShoppingItems(name: String, isSelected:Bool, imageData:Data) {
+        let shoppingItem = ShoppingItem(name: name, isSelected: isSelected, imageData: imageData)
+        shoppingItems.append(shoppingItem)
+        
+    }
+    
+    func loadShoppingItems() {
+        
+    }
     
     
     
