b1cbca045209ae2756905bb110094b828cc4da18
Lotanna <sugabelly@gmail.com>
2018-10-19 14:14:05 -0400

    showing selected items

--- Shopping List/GetNotifiedController.swift
+++ Shopping List/GetNotifiedController.swift
@@ -11,4 +11,25 @@ import UIKit
 
 class GetNotifiedController: UIViewController {
     
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        changeMessage()
+        
+    }
+    
+    //Change the message to match the items selected
+    
+    var itemController: ItemController?
+    
+    func changeMessage(){
+    guard let itemController = itemController else { return }
+    let selectedItems = itemController.selections.count
+    messageLabel.text = "So far, you've picked out  \(selectedItems) items!"
+}
+    
 }
