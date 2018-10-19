83f33999b349636edc2cab577a5588aa3f0fe74d
Paul Yi <yipaulc@gmail.com>
2018-10-19 09:07:46 -0700

    Add getAuthorizationStatus and requestAuthorization functions to LocalNotificationHelper class

--- Shopping List/LocalNotificationHelper.swift
+++ Shopping List/LocalNotificationHelper.swift
@@ -3,4 +3,30 @@ import UserNotifications
 
 class LocalNotificationHelper: NSObject, UNUserNotificationCenterDelegate {
     
+    // Copy pasta
+    func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                completion(settings.authorizationStatus)
+            }
+        }
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if let error = error { NSLog("Error requesting authorization status for local notifications: \(error)") }
+            
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
