b3a3790a1bf112c7488a53d660ef4dd201ec84aa
Lotanna <sugabelly@gmail.com>
2018-10-19 12:36:25 -0400

    Set up controllers

---
+++ Shopping List/GetNotifiedController.swift
@@ -0,0 +1,10 @@
+//
+//  GetNotifiedController.swift
+//  Shopping List
+//
+//  Created by Lotanna Igwe-Odunze on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
--- Shopping List/ShoppingListController.swift
+++ Shopping List/ShoppingListController.swift
@@ -1,15 +1,15 @@
-//
+// ShoppingListController.swift
 //  Lotanna iOS3 Sprint 2 Challenge begins!
 
 import UIKit
 
-class ViewController: UIViewController {
+class ShoppingListController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Registering the cell class
-        
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "groceryCell")
         
     }
 
