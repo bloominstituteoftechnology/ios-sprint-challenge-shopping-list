d4e52cd12a125d1b6db250dbe84169bc468739da
Rwoltx <richardx1016@gmail.com>
2018-10-19 12:06:17 -0400

    Created ViewControllers

---
+++ Shopping List/ViewControllers/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Rick Wolter on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
