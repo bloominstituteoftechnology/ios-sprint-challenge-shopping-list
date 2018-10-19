15a915efc5b78e741e4633d0a71b37faf370d7ba
Jerrick Warren <jerrickwarren@MacBook-Pro-3.local>
2018-10-19 08:46:14 -0700

    created model and controller

---
+++ Shopping List/Model + Controller/ShoppingItem.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Jerrick Warren on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    
+    var name: String
+    var isSelected: Bool
+    var imageData: Data
+    
+    init(name: String, isSelected:Bool, imageData: Data) {
+        self.name = name
+        self.isSelected = false
+        self.imageData = imageData
+    }
+    
+}
---
+++ Shopping List/Model + Controller/ShoppingItemController.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Jerrick Warren on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingItemContorller {
+    
+    
+    
+    
+}
