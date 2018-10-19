457bd2048bc631fd939362bd44b81111fddb008e
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-19 11:43:37 -0400

    initial commit - IB

---
+++ Shopping List/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Yvette Zhukovsky on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Shopping List/ShoppingUIViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShoppingUIViewController.swift
+//  Shopping List
+//
+//  Created by Yvette Zhukovsky on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
