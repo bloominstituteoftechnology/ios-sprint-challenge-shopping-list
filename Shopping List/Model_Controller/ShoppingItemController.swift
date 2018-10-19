0124ee6bfb9fa283c3dcfd3abd4390c55aa5fbd6
Rwoltx <richardx1016@gmail.com>
2018-10-19 12:01:35 -0400

    UI completeted and Model started

---
+++ Shopping List/Model_Controller/ShoppingItemController.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Rick Wolter on 10/19/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingItemController {
+    
+    
+    
+    
+    
+    private (set) var shoppingItems: [ShoppingItem] = []
+}
