006304f7d6cceacfa24e41e1bf9e54b7ba5a75e9
Frulwinn <Frulwinn@gmail.com>
2018-10-19 10:22:37 -0700

    Added outlets on ShoppingListViewController

--- Shopping List/AppDelegate.swift
+++ Shopping List/AppDelegate.swift
@@ -1,46 +1,71 @@
-//
-//  AppDelegate.swift
-//  Shopping List
-//
-//  Created by Spencer Curtis on 8/3/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
-//
-
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        checkNotificationAuthorization()
         return true
+        
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func checkNotificationAuthorization() {
+        //see whether the user has already granted permission
+        // and if not, start request process
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        center.getNotificationSettings { settings in
+            guard settings.notificationCenterSetting != .enabled else {
+                NSLog("location notifications has been granted")
+                return
+            }
+            
+            // Handle ungranted notification permissions
+            self.requestNotificationAuthorization()
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func requestNotificationAuthorization() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            
+            if let error = error {
+                NSLog("Error requestion notification authorization: \(error)")
+                
+                return
+            }
+            NSLog("notification authorization status: \(granted ? "granted" : "notgranted")")
+            
+            guard !granted else {return}
+            
+            let alertController = UIAlertController(
+                title: "Authorization not granted",
+                message: "This app cannot present notifications without your explicit consent",
+                preferredStyle: .alert)
+            
+            let okayAction = UIAlertAction(title: "Understood", style: .default, handler: nil)
+            let settingsAction = UIAlertAction(title: "Open Settings", style: .default, handler: { action in
+                guard let url = URL(string: UIApplication.openSettingsURLString) else {
+                    fatalError("Application-supplied openSettingsURLString failed to create cromulent URL")
+                }
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            })
+            
+            alertController.addAction(okayAction)
+            alertController.addAction(settingsAction)
+            
+            self.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+            
+        }
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        NSLog("Will present notification: \(notification)")
+        completionHandler(.alert)
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
---
+++ Shopping List/ShoppingListViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ShoppingListViewController : UITableViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBAction func sendOrderButton(_ sender: Any) {
+        
+        
+    }
+}
