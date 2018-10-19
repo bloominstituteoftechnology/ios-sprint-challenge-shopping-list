b4529d9c7f91dc7dac0335fa20fb9ce20ba80e03
Rwoltx <richardx1016@gmail.com>
2018-10-19 13:03:42 -0400

    Completed ShoppingItemCollectionViewCell

--- Shopping List/ViewControllers/ShoppingItemCollectionViewCell.swift
+++ Shopping List/ViewControllers/ShoppingItemCollectionViewCell.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var isInCartLabel: UILabel!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+    
+    
+    var item: ShoppingItem? {
+        didSet {
+           updateViews()
+        }
+    }
+    
+    
+    
+    
+    // update the cell
+    func updateViews(){
+        guard let item = item else {return}
+        
+        itemNameLabel.text = item.name
+        isInCartLabel.text = item.isInList ? "Added" : "Not Added"
+        itemImage.image = UIImage(data: item.image)
+        
+    
+    }
+    
+    
+    
+    
 }
