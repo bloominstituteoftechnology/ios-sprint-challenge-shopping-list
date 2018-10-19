973dd358bdc5e1dad06856e78a1008e6ed31ac75
Paul Yi <yipaulc@gmail.com>
2018-10-19 08:56:05 -0700

    Write persistence functions in ShoppingItemController

--- Shopping List/ShoppingItemController.swift
+++ Shopping List/ShoppingItemController.swift
@@ -3,10 +3,37 @@ import UIKit
 class ShoppingItemController {
     
     var shoppingItems: [ShoppingItem] = []
-    
+  
     private var shoppingListURL: URL? {
         let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
         let fileName = "shoppinglist.plist"
         return documentDirectory?.appendingPathComponent(fileName)
     }
+    
+    // Persistence Functions
+    private func saveToPersistentStore() {
+        let plistEncoder = PropertyListEncoder()
+        
+        do {
+            let shoppingListData = try plistEncoder.encode(shoppingItems)
+            guard let shoppingListURL = shoppingListURL else { return }
+            try shoppingListData.write(to: shoppingListURL)
+        } catch {
+            NSLog("Error encoding shoppping list: \(error)")
+        }
+    }
+    
+    private func loadFromPersistentStore() {
+        do {
+            guard let shoppingListURL = shoppingListURL,
+                FileManager.default.fileExists(atPath: shoppingListURL.path) else { return }
+            let shoppingListData = try Data(contentsOf: shoppingListURL)
+            let plistDecoder = PropertyListDecoder()
+            self.shoppingItems = try plistDecoder.decode([ShoppingItem].self, from: shoppingListData)
+        } catch {
+            NSLog("Error decoding shopping list: \(error)")
+        }
+    }
+    
+    
 }
