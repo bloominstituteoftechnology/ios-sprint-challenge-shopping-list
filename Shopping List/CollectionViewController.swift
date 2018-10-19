51cb29c2d8cd20f9d50908052ccbf8c98295ce48
Frulwinn <Frulwinn@gmail.com>
2018-10-19 11:04:07 -0700

    Fixing errors on CollectionViewController

--- Shopping List/CollectionViewController.swift
+++ Shopping List/CollectionViewController.swift
@@ -16,21 +16,37 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     let targetDimension: CGFloat = 320
     
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
         
-        guard let layout = collectionView?.collectionViewLayout as?
+        
+        guard let layout = collectionView.collectionViewLayout as?
             UICollectionViewFlowLayout else {
             fatalError("Unable to retrieve layout")
         }
-        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
                 reuseIdentifier, for: indexPath) as? CollectionViewCell else {
                 fatalError("Inconsistent view state")
             }
             
-            cell.imageView.image = image[indexPath.row]
             return cell
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            // Fetch image
+            let image = images[indexPath.row]
+            
+            // Fetch largest dimension of the image, whether width or height
+            let maxDimension = max(image.size.width, image.size.height)
+            
+            // Calculate how to scale that largest dimension into `targetDimension`
+            let scale = targetDimension / maxDimension
+            
+            // Return scaled dimensions
+            return CGSize(width: image.size.width * scale, height: image.size.height * scale)
         }
     }
 }
