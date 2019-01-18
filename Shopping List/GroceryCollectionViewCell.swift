602d8365e704f6c504634db8a693e59e55ae88e1
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-18 08:37:53 -0800

    Create model object.

---
+++ Shopping List/GroceryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GroceryCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Nathanael Youngren on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class GroceryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Nathanael Youngren on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem {
+    var image: UIImage
+    var name: String
+    var isAdded: Bool
+    
+    init(image: UIImage, name: String, isAdded: Bool = false) {
+        self.image = image
+        self.name = name
+        self.isAdded = isAdded
+    }
+}
