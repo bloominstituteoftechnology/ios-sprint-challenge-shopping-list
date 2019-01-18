8bb0fd1cbeafd2e497fdac74d3460e6d64a549f7
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-18 09:09:53 -0800

    storyboard set up, setting up model and controller

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Moses Robinson on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem: Equatable {
+    
+    let name: String
+    let image: UIImage
+    let isAdded: Bool
+    
+    init(name: String, image: UIImage, isAdded: Bool = false) {
+        (self.name, self.image, self.isAdded) = (name, image, isAdded)
+    }
+}
