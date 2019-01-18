60f8d0694b033315384235dec1cc5e85087914d6
Dustin Koch <dustin.koch@me.com>
2019-01-18 10:21:39 -0600

    Model and first shopping item controller complete

---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Dustin Koch on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem {
+    var image: UIImage
+    var nameOfItem: String
+    var addedToCart: Bool
+    
+    init(image: String, nameOfItem: String, addedToCart: Bool = false) {
+        self.image = UIImage(named: image)!
+        self.nameOfItem = nameOfItem
+        self.addedToCart = addedToCart
+    }
+}
