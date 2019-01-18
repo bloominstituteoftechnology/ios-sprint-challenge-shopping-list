baa2e631572a3f3f0cf182a27b25be007fe0ae3d
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-18 04:12:57 -0500

    Add controller

---
+++ Shopping List/ShoppingKey.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingKey.swift
+//  Shopping List
+//
+//  Created by Nelson Gonzalez on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static var isInitiatedKey = "isInitiated"
+}
