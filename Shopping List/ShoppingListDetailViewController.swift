76739217746e2c57445956f838909acc492df994
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-18 11:08:55 -0800

    application functionality complete, needs userdefaults

--- Shopping List/ShoppingListDetailViewController.swift
+++ Shopping List/ShoppingListDetailViewController.swift
@@ -9,8 +9,38 @@
 import UIKit
 
 class ShoppingListDetailViewController: UIViewController {
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        updateViews()
+    }
+    
+    @IBAction func sendOrderButtonTapped(_ sender: Any) {
+        guard let name = nameTextField.text, !name.isEmpty,
+        let address = addressTextField.text, !address.isEmpty,
+        let shoppingList = shoppingListController?.addedShoppingList, !shoppingList.isEmpty else  { return }
+        
+        print("Hello \(name), your \(shoppingList.count) item(s) are being sent to: \(address)")
+    }
+    
+    private func updateViews() {
+        guard let shoppingList = shoppingListController?.addedShoppingList else  { return }
+        
+        textLabel.text = "You currently have (\(shoppingList.count)) item(s) in your shopping cart."
+    }
+    
     //MARK: - Properties
 
     var shoppingListController: ShoppingItemController?
+    
+    var shoppingList: [ShoppingItem]? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
 }
