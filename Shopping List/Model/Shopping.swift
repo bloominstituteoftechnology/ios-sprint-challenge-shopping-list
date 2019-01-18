e4a9f3da5f08c208e17d6a489c1e41a37e9e4ad1
Sean Doyle <sdoylelambda@gmail.com>
2019-01-18 11:57:21 -0500

    Image, name and if it's added storage ability added.

--- Shopping List/Model/Shopping.swift
+++ Shopping List/Model/Shopping.swift
@@ -1,5 +1,14 @@
-import Foundation
+import UIKit
 
 struct Shopping {
     
+    let image: UIImage
+    let name: String
+    var like: Bool
+    
+    init(image: UIImage, name: String, like: Bool) {
+        self.image = image
+        self.name = name
+        self.like = like
+    }
 }
