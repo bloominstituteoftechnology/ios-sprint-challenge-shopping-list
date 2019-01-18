6b49c40a32fadeaa47619d245ef81c481d69efba
Stuart Sippel <stuartsippel@icloud.com>
2019-01-18 10:51:03 -0600

    Build UI, model

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Stuart on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingItem {
+    var name: String
+    var imageData: Data
+    var isInShoppingList: Bool
+    
+    init(name: String, imageData: Data, isInShoppingList: Bool) {
+        self.name = name
+        self.imageData = imageData
+        self.isInShoppingList = isInShoppingList
+    }
+}
---
+++ Shopping List/ShoppingItemController.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Stuart on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingItemController {
+    
+}
