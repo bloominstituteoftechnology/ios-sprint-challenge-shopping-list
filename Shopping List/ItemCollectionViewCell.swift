a59a70f0d2d21bbf02b9897b961973ef4d8a4b43
Vijay Das <vijaytdas@gmail.com>
2019-01-25 11:26:45 -0600

    add ItemCollectionViewCell outlets, userDefaults, didSet and updateViews()

--- Shopping List/ItemCollectionViewCell.swift
+++ Shopping List/ItemCollectionViewCell.swift
@@ -10,4 +10,54 @@ import UIKit
 
 class ItemCollectionViewCell: UICollectionViewCell {
     
+  
+    let userDefaults = UserDefaults.standard
+    
+    // outlets
+    
+    @IBOutlet weak var addedLabel: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    
+    
+    // didSet
+    // updateViews with userdefaults
+    
+    var item: ShoppingItem? {
+        didSet {
+            updateViews()
+            
+        }
+    }
+    
+    private func updateViews() {
+        
+        if let item = item {
+            itemLabel.text = item.name
+            
+            guard let image = UIImage(named: item.image) else { return }
+            
+            itemImage.image = image
+            
+            if item.isAdded {
+                addedLabel.text = "Added"
+                
+            } else {
+                addedLabel.text = "Not Added"
+            }
+            
+            let addedBool = item.isAdded
+            
+            userDefaults.set(addedBool, forKey: .isAddedToCartKey)
+            
+        
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
 }
