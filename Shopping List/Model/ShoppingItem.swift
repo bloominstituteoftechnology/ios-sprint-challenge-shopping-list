590d6e6ef01132e996d37395c34cbb258b0a84ee
Paul Yi <yipaulc@gmail.com>
2019-01-25 08:05:08 -0800

    Create ShoppingItem model object

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Paul Yi on 1/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    var name: String
+    var image: String
+    var isAdded: Bool
+}
