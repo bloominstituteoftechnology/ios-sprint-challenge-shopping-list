72a296ff54af6f9edd0ba4b86444582cb006dfb6
Paul Yi <yipaulc@gmail.com>
2019-01-25 09:25:18 -0800

    Set up ShoppingDetailViewController

--- Shopping List/View Controllers/ShoppingDetailViewController.swift
+++ Shopping List/View Controllers/ShoppingDetailViewController.swift
@@ -9,32 +9,38 @@
 import UIKit
 
 class ShoppingDetailViewController: UIViewController {
+    var shoppingItemController: ShoppingItemController? {
+        didSet {
+            updateViews()
+        }
+    }
     
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     
     @IBAction func sendOrder(_ sender: Any) {
+        guard let name = nameTextField.text,
+            let address = addressTextField.text else { return }
     }
     
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let shoppingItems = shoppingItemController?.shoppingItems else { return }
+        
+        var numberOfItems = 0
+        for item in shoppingItems {
+            if item.isAdded {
+                numberOfItems += 1
+            }
+        }
+        
+        textLabel?.text = "You currently have \(numberOfItems) items(s) in your shopping cart."
     }
-    */
 
 }
--- Shopping List/View Controllers/ShoppingListCollectionViewController.swift
+++ Shopping List/View Controllers/ShoppingListCollectionViewController.swift
@@ -20,15 +20,15 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     
     
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        guard let detailVC = segue.destination as? ShoppingDetailViewController else { return }
+        
+        detailVC.shoppingItemController = shoppingItemController
     }
-    */
+ 
 
     // MARK: UICollectionViewDataSource
 
