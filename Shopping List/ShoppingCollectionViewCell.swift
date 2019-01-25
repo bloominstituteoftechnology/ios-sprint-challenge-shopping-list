253a4bdb8ab8d0735a8126bb929bd6d1f2edc309
Sean Doyle <sdoylelambda@gmail.com>
2019-01-25 11:25:26 -0500

    View controllers with outlet connections.

---
+++ Shopping List/ShoppingCollectionViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ShoppingCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var ImageFoodOutlet: UIImageView!
+    @IBOutlet weak var addOrNotLabel: UILabel!
+    @IBOutlet weak var foodNameLabel: UILabel!
+    
+}
