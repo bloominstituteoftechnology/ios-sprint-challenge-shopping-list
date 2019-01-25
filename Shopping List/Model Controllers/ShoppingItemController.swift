addd288e30b28cceeeaffc3e091ff6709d1e9e5f
Vijay Das <vijaytdas@gmail.com>
2019-01-25 11:02:55 -0600

    add createItems() method to create array of shopping items from provided names and images

--- Shopping List/Model Controllers/ShoppingItemController.swift
+++ Shopping List/Model Controllers/ShoppingItemController.swift
@@ -9,6 +9,31 @@
 import UIKit
 
 class ShoppingItemController {
+    // properties
+    // need to implement:
+
+    // updateIsAdded method
+    // filter methods for added and notadded items
+    // persistence
+    //
+    
+    // properties
+    
+    private(set) var shoppingList: [ShoppingItem] = []
+    
+    // create initial items and initialize
+    // use provided snippet: let itemNames = ["apple", "grapes", "milk", "muffin", "popcorn", "soda", "strawberries"]
+    
+    func createItems() {
+        let itemNames = ["apple", "grapes", "milk", "muffin", "popcorn", "soda", "strawberries"]
+        for name in itemNames {
+            let item = ShoppingItem(image: name, name: name)
+            shoppingList.append(item)
+        }
+        
+    }
+    
+    
     
     
 }
