b8ee5a51fd8fac716bdb869b547c3d7f2f4d8415
Vijay Das <vijaytdas@gmail.com>
2019-01-25 11:41:35 -0600

    add SettingsKeys, implement methods for numberOfItems and cellForItem in ShoppingListCollectionViewController

--- Shopping List/View Controllers/ShoppingListCollectionViewController.swift
+++ Shopping List/View Controllers/ShoppingListCollectionViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "ItemCell"
+
+let shoppingItemController = ShoppingItemController()
 
 class ShoppingListCollectionViewController: UICollectionViewController {
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView?.reloadData()
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +29,7 @@ class ShoppingListCollectionViewController: UICollectionViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "reuseIdentifier")
 
         // Do any additional setup after loading the view.
     }
@@ -36,23 +46,24 @@ class ShoppingListCollectionViewController: UICollectionViewController {
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
+ 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return shoppingItemController.shoppingList.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
+        
+        guard let itemCell = cell as? ItemCollectionViewCell else { return cell }
+        
+        let item = shoppingItemController.shoppingList[indexPath.row]
+        
+        itemCell.item = item
+        
+        return itemCell
+        
+
     }
 
     // MARK: UICollectionViewDelegate
