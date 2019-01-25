42f2fc80aea26e09d28a74a90fbe4696a79cfe69
Paul Yi <yipaulc@gmail.com>
2019-01-25 08:59:31 -0800

    Create storyboard and connect outlets

---
+++ Shopping List/View Controllers/ShoppingDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ShoppingDetailViewController.swift
+//  Shopping List
+//
+//  Created by Paul Yi on 1/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingDetailViewController: UIViewController {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBAction func sendOrder(_ sender: Any) {
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Paul Yi on 1/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var addedNotAddedLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
