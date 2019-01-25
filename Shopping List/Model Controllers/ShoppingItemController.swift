35f14e1100c8ee1db7f583fb2bc9e97315b45b3b
Vijay Das <vijaytdas@gmail.com>
2019-01-25 10:29:43 -0600

    add skeleton for ShoppingItemController

---
+++ Shopping List/Model Controllers/ShoppingItemController.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Vijay Das on 1/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemController {
+    
+    
+}
