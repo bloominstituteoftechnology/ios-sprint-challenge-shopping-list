ac3b2690dff589c65add44ba8036a52d2320322d
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-03 12:30:37 -0400

    Set up UI

---
+++ Shopping List/View Controllers/OrderingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  OrderingViewController.swift
+//  Shopping List
+//
+//  Created by Lisa Sampson on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class OrderingViewController: UIViewController {
+    
+    @IBOutlet weak var orderLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendOrderButtonTapped(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Lisa Sampson on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var isAddedLabel: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
