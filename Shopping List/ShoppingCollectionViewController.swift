351232ce74eac1864677bc2a649c288f4b5a88af
Michael Redig <mredig@gmail.com>
2019-05-03 12:02:02 -0500

    added activity label

--- Shopping List/ShoppingCollectionViewController.swift
+++ Shopping List/ShoppingCollectionViewController.swift
@@ -9,25 +9,36 @@
 import UIKit
 
 
-class ShoppingCollectionViewController: UICollectionViewController {
+class ShoppingCollectionViewController: UIViewController {
+	@IBOutlet var activityLabel: UILabel!
 	
 	let shoppingItemController = ShoppingItemController()
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		
+		activityLabel.alpha = 0
 	}
 	
+	func updateActivityLabel(withItem item: ShoppingItem) {
+		// new status is opposite the status of the item passed in
+		let text = !item.inCart ? "Added \(item.imageName) to cart" : "Removed \(item.imageName) from cart"
+		
+		activityLabel.text = text
+		activityLabel.alpha = 1.0
+		UIView.animate(withDuration: 5.0) {
+			self.activityLabel.alpha = 0
+		}
+	}
 }
 
 
 //MARK:- collection view data and delegate stuff
-extension ShoppingCollectionViewController {
-	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension ShoppingCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return shoppingItemController.shoppingItems.count
 	}
 	
-	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShoppingCell", for: indexPath)
 		let shoppingCell = cell as? ShoppingItemCollectionCell
 		shoppingCell?.shoppingItem = shoppingItemController.shoppingItems[indexPath.item]
@@ -36,9 +47,10 @@ extension ShoppingCollectionViewController {
 		return cell
 	}
 	
-	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		let item = shoppingItemController.shoppingItems[indexPath.item]
 		shoppingItemController.updateShoppingItem(item, inCart: !item.inCart)
 		collectionView.reloadItems(at: [indexPath])
+		updateActivityLabel(withItem: item)
 	}
 }
