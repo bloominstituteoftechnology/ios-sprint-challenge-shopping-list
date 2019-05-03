b297a4e0505eb54d9df2d986822cdcc7b7761f61
victor <ruizdvictor@gmail.com>
2019-05-03 14:16:36 -0400

    added error handling

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -24,6 +24,22 @@ class ViewController: UIViewController {
     
     @IBAction func sendButtonPressed(_ sender: Any) {
         //pop alert
+        guard let shoppingItemController = shoppingItemController else {return}
+        let numberOfItems = shoppingItemController.shoppingItems.filter({$0.isAddedToList}).count
+        guard let name = nameTextField.text, name != "", let address = addressTextField.text, address != "", numberOfItems > 0 else {
+            
+            let alert = UIAlertController(title: "Order Invalid", message: "Please make sure you have name, address, and have at least one item. Thank You", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+    
+            return
+        }
+        let alert = UIAlertController(title: "Delivery For \(name)!", message: "Your items will be delivered to \(address)", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+        
     }
     
     private func updateViews() {
