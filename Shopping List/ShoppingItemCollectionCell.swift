c00b86445476a25c34530cafe2d8b23c3d4c8543
Michael Redig <mredig@gmail.com>
2019-05-03 13:55:06 -0500

    added colored images

--- Shopping List/ShoppingItemCollectionCell.swift
+++ Shopping List/ShoppingItemCollectionCell.swift
@@ -27,7 +27,8 @@ class ShoppingItemCollectionCell: UICollectionViewCell {
 		itemImageView.layer.cornerRadius = 15
 		guard let item = shoppingItem else { return }
 		itemLabel.text = item.imageName.capitalized
-		itemImageView.image = UIImage(named: item.imageName)
+		let filename = item.inCart ? "\(item.imageName) 2" : item.imageName
+		itemImageView.image = UIImage(named: filename)
 		itemImageView.alpha = item.inCart ? 1.0 : 0.2
 	}
 }
