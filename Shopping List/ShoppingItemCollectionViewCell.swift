e5c0b154280f7b5a232cdc1c7af93f078775820d
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-03 08:37:44 -0700

    ShoppingItemCollectionViewController and cell setup

---
+++ Shopping List/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Hector Steven on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+
+
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var notAddedLabel: UILabel!
+
+
+}
