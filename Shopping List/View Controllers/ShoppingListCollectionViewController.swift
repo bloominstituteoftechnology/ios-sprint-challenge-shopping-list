c42fe3d7eb4decd820d39153437eb8f759ee929c
alexladines <alexladines@outlook.com>
2019-05-03 10:56:27 -0700

    Finish ShoppingListCollectionViewController, only thing left is to fix UserDefaults/Data Persistence in ShoppingListController.

--- Shopping List/View Controllers/ShoppingListCollectionViewController.swift
+++ Shopping List/View Controllers/ShoppingListCollectionViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class ShoppingListCollectionViewController: UICollectionViewController {
 
     // MARK: - Properties
@@ -17,16 +15,57 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     
     // MARK: - ShoppingListCollectionViewController
     
+    func updateView() {
+        self.collectionView?.reloadData()
+    }
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    // MARK: - Navigation
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.updateView()
+    }
     // MARK: UICollectionViewDataSource
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.shoppinglistController.shoppingList.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Item", for: indexPath) as! ShoppingListCollectionViewCell
+        
+        let item = self.shoppinglistController.shoppingList[indexPath.item]
+        
+        cell.item = item
+        
+        return cell
+    }
+    
     // MARK: UICollectionViewDelegate
+    // When user taps an item
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.shoppinglistController.updateShoppingItem(item: self.shoppinglistController.shoppingList[indexPath.item])
+        self.updateView()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        self.shoppinglistController.updateShoppingItem(item: self.shoppinglistController.shoppingList[indexPath.item])
+        self.updateView()
+    }
+    
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SendOrder"
+        {
+            guard let shoppingListDetailVC = segue.destination as? ShoppingListDetailViewController else { return }
+
+            shoppingListDetailVC.shoppinglistController = self.shoppinglistController
+        }
+    }
+
 
 
 }
