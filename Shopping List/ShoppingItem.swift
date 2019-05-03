7073f53fe9667b17a4828accee91c93611ae71fe
Christopher Aronson <christopheraronson@icloud.com>
2019-05-03 08:20:35 -0700

    Created ShoppingItem Class

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Christopher Aronson on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem {
+    var image: UIImage
+    var title: String
+    var added: Bool
+}
