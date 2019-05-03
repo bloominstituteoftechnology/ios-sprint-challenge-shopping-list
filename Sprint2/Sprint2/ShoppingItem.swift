acee3dd54e13dab67030577304722ff47bf4fa2d
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MBP.frontier.com>
2019-05-03 11:58:17 -0400

    Model complete

---
+++ Sprint2/Sprint2/ShoppingItem.swift
@@ -0,0 +1,26 @@
+//
+//  ShoppingItem.swift
+//  Sprint2
+//
+//  Created by Diante Lewis-Jolley on 5/3/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ShoppingItem: Equatable, Codable {
+
+    var name: String
+    var isliked: Bool
+
+    init(name: String, isliked: Bool = false) {
+        self.name = name
+        self.isliked = isliked
+        var image: UIImage {
+            return UIImage(named: name)!
+        }
+    }
+
+
+}
