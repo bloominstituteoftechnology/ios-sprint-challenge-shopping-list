987d2eadfcb7d68506f14c67aaca720b4495fb96
Michael Redig <mredig@gmail.com>
2019-05-03 12:12:51 -0500

    added order submission vc

---
+++ Shopping List/OrderSubmissionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  OrderSubmissionViewController.swift
+//  Shopping List
+//
+//  Created by Michael Redig on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+
+class OrderSubmissionViewController: UIViewController {
+	
+	@IBOutlet var orderInfoLabel: UILabel!
+	@IBOutlet var nameTextField: UITextField!
+	@IBOutlet var addressTextField: UITextField!
+	@IBOutlet var submitOrderButton: UIButton!
+	
+	
+	
+	
+	@IBAction func submitOrderButtonPressed(_ sender: UIButton) {
+	}
+	
+	
+	
+}
