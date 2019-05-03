45c06923470e5e597f62036b4df0e420a68ae3d7
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MBP.frontier.com>
2019-05-03 11:51:35 -0400

    hooked up UI to viewControllers

--- Sprint2/Sprint2/DetailViewController.swift
+++ Sprint2/Sprint2/DetailViewController.swift
@@ -15,16 +15,14 @@ class DetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+
+
+    
+    @IBAction func sendOrderButtonTapped(_ sender: Any) {
+    }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    @IBOutlet weak var likeItemsLabel: UILabel!
 
 }
---
+++ Sprint2/Sprint2/ItemCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCollectionViewCell.swift
+//  Sprint2
+//
+//  Created by Diante Lewis-Jolley on 5/3/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+
+    @IBOutlet weak var likeButtonTapped: UIButton!
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    
+}
