a5bc30f7e0a7f5aeeb2f6968414e2636938ed086
Kobe McKee <kobe.mckee@gmail.com>
2019-05-03 09:44:15 -0600

    Initial file setup and UI setup. Created ShoppingItem Model and ShoppingList class.

---
+++ Shopping List/ShoppingListController.swift
@@ -0,0 +1,28 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Kobe McKee on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingList {
+    
+    var shoppingItems: [ShoppingItem] = [ShoppingItem(itemName: "apple", imageName: "apple"),
+                                         ShoppingItem(itemName: "grapes", imageName: "grapes"),
+                                         ShoppingItem(itemName: "milk", imageName: "milk"),
+                                         ShoppingItem(itemName: "muffin", imageName: "muffin"),
+                                         ShoppingItem(itemName: "popcorn", imageName: "popcorn"),
+                                         ShoppingItem(itemName: "soda", imageName: "soda"),
+                                         ShoppingItem(itemName: "strawberries", imageName: "strawberries")]
+    
+    var shoppingList: [ShoppingItem] = []
+    
+    
+    
+    
+    
+    
+}
