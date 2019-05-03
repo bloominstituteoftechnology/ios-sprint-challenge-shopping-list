3352bcc04889ed18081cbc6dc7705c557f2579ac
Jeffrey Carpenter <spaz926@me.com>
2019-05-03 12:22:36 -0500

    Add alert to submit order screen

--- Shopping List/ShoppingCartViewController.swift
+++ Shopping List/ShoppingCartViewController.swift
@@ -23,6 +23,17 @@ class ShoppingCartViewController: UIViewController {
     }
     
     @IBAction func submitOrderButtonPressed(_ sender: UIButton) {
+        
+        guard let address = addressTextField.text,
+        !address.isEmpty,
+        let name = nameTextField.text,
+        !name.isEmpty
+        else { return }
+        
+        let alert = UIAlertController(title: "Order for \(name)", message: "Your items will be delivered to \(address) in 15 minutes.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     private func updateViews() {
@@ -31,9 +42,5 @@ class ShoppingCartViewController: UIViewController {
         
         let numberOfItemsInCart = shoppingItemController.shoppingItems.filter({ $0.isInCart }).count
         shoppingListLabel.text = "You have \(numberOfItemsInCart) item(s) in your shopping list."
-    }
-    
-    private func showAlert() {
-        
     }
 }
