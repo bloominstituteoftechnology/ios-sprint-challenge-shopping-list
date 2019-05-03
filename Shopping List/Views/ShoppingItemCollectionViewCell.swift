23770c71146268ad24e70c898cc765775f1bfcc1
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-03 12:35:06 -0400

    Configure the cell

--- Shopping List/Views/ShoppingItemCollectionViewCell.swift
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - Properties and Outlets
     @IBOutlet weak var isAddedLabel: UILabel!
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var itemLabel: UILabel!
     
+    var shoppingItem: ShoppingItem? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - View Loading Methods
+    
+    func updateViews() {
+        guard let shoppingItem = shoppingItem else { return }
+        
+        itemImage.image = UIImage(named: shoppingItem.image)
+        itemLabel.text = shoppingItem.item
+        
+        if shoppingItem.isAdded == true {
+            isAddedLabel.text = "Added"
+        } else {
+            isAddedLabel.text = "Not Added"
+        }
+    }
 }
