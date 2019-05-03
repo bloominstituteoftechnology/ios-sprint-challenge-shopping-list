9d81bdee70c28bff06bb759feab7c19eb0d9d748
Dan Morse <morsedan@outlook.com>
2019-05-03 14:03:27 -0700

    Added different message for empty name and address fields.

--- Shopping List/View Controllers/ShoppingItemDetailViewController.swift
+++ Shopping List/View Controllers/ShoppingItemDetailViewController.swift
@@ -28,7 +28,9 @@ class ShoppingItemDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if itemCount == 1 {
+        if itemCount == 0 {
+            
+        } else if itemCount == 1 {
             listCountLabel.text = "You currently have 1 item in your\nshopping list"
         } else {
             listCountLabel.text = "You currently have \(itemCount) items in your\nshopping list"
@@ -36,7 +38,13 @@ class ShoppingItemDetailViewController: UIViewController {
     }
     
     @IBAction func sendOrderButtonTapped(_ sender: Any) {
+        
         guard let name = nameTextField.text, let address = addressTextField.text else { return }
+        if name.isEmpty || address.isEmpty {
+            let alert = UIAlertController(title: "Sorry!", message: "We cannot deliver your items without your name and address. Enter them now.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true)
+        }
         let alert = UIAlertController(title: "Delivery for \(name)!", message: "Your shopping items will be delivered to \(address) in 15 minutes!", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         present(alert, animated: true)
