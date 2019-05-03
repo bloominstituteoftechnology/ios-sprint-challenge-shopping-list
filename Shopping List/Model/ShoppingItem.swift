0a20cb7cbfb4c028b1a848d0ad5f54cd908d167c
alexladines <alexladines@outlook.com>
2019-05-03 08:51:20 -0700

    Organize project files.

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Alex Ladines on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Shopping List/Model/ShoppingItemController.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Alex Ladines on 5/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
