db326b2165a83233f379cd82de016e9405625d2b
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-10 11:31:46 -0400

    Created Storyboard and files for corresponding view controllers and cells.

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Thomas Cacciatore on 5/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct  ShoppingItem {
+    var itemName: String
+    var hasBeenAdded: Bool
+}
+
+
---
+++ Shopping List/ShoppingListController.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Thomas Cacciatore on 5/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingList {
+    
+    
+    
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+
+}
