7e4466839ccc03a1bac3ec3de5f21af8e08c9efa
John Pitts <jgpitts@gmail.com>
2019-05-10 13:06:40 -0400

    wrote properties and methods for Cell file, next will write delegate to rid the error from calling delegate

--- Shopping List/VIEWS/SkuCollectionViewCELL.swift
+++ Shopping List/VIEWS/SkuCollectionViewCELL.swift
@@ -8,6 +8,60 @@
 
 import UIKit
 
+
 class SkuCollectionViewCELL: UICollectionViewCell {
+    
+    
+    @IBAction func booleanButtonTapped(_ sender: Any) {
+        
+        delegate?.toggleSkuInCart(for: self)
+    }
+    
+    func updateViews() {
+        
+        skuLabel.text = sku?.skuName
+        skuLabel.textColor = .red
+        
+        imageView.image = UIImage(named: sku!.skuImageName)
+        
+        //if skuInCart is properly stored, this will unlock the previously stored value
+        
+        if sku?.skuInCart == true {
+            booleanButtonLabel.titleLabel?.text = "Added"
+            print("ADDED!!!!!")
+        } else {
+            booleanButtonLabel.titleLabel?.text = "not added"
+            print("NOT")
+        }
+    }
+    /*
+     //UnNEEDED bc collectionView already sets these & they don't change
+     //update sku's name & image
+     //imageView.image = UIImage(named: sku.skuImageName)
+     //skuLabel.text = sku.skuName
+     
+     if sku?.skuInCart == true {
+     booleanButtonLabel.titleLabel?.text = "Added"
+     
+     } else {
+     booleanButtonLabel.titleLabel?.text = "Not Added"
+     
+     }
+     */
+    
+    // PROPERTIES
+    
+    @IBOutlet weak var booleanButtonLabel: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var skuLabel: UILabel!
+    
+    var sku: Sku? {
+        didSet {
+            updateViews()
+        }
+    }
+    var delegate: SkuCollectionViewCELLDelegate?
+    
+    var skuController = SkuController()
     
 }
