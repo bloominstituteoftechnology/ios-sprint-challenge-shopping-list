ebc1784176ebb549d0267cdd0e42c99297abbd31
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-10 11:23:51 -0400

    Views and View Controllers
    Views and View Controllers configured for respective storyboard elements

---
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Jordan Davis on 5/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
