029f7128ca6f6199d0ef98a1872d339463e5e30b
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-20 16:23:19 -0400

    recent

--- Shopping List/ShoppingListCollectionViewController.swift
+++ Shopping List/ShoppingListCollectionViewController.swift
@@ -18,15 +18,7 @@ class ShoppingListCollectionViewController: UICollectionViewController {
         super.viewWillAppear(animated)
         self.collectionView.reloadData()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
     // MARK: UICollectionViewDataSource
 
@@ -49,21 +41,6 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PlanetCollectionViewCell
-     
-     let planet = self.planetController.planets[indexPath.row]
-     cell.label.text = planet.name
-     cell.imageView.image = planet.image
-     
-     return cell
-     }
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
 
     /*
     // Uncomment this method to specify if the specified item should be selected
