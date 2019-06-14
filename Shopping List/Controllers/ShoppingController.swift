597b258247100183813ccab2e8f57ae0bb393db7
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-14 12:32:49 -0400

    Setting up ShoppingController for persistence, and setting up storyboard.

---
+++ Shopping List/Controllers/ShoppingController.swift
@@ -0,0 +1,63 @@
+//
+//  ShoppingController.swift
+//  Shopping List
+//
+//  Created by Marlon Raskin on 6/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingController {
+	
+	private(set) var items: [ShoppingItem] = []
+	
+	var showInitialShoppingItems = [
+	ShoppingItem(itemName: "Apple", hasBeenAdded: false),
+	ShoppingItem(itemName: "Grapes", hasBeenAdded: false),
+	ShoppingItem(itemName: "Milk", hasBeenAdded: false),
+	ShoppingItem(itemName: "Muffins", hasBeenAdded: false),
+	ShoppingItem(itemName: "Popcorn", hasBeenAdded: false),
+	ShoppingItem(itemName: "Soda", hasBeenAdded: false),
+	ShoppingItem(itemName: "Strawberries", hasBeenAdded: false)
+	]
+	
+	let shouldShowShoppingItems = UserDefaults.standard.bool(forKey: .shouldShowSoppingItems)
+
+	private var persistentFileURL: URL? {
+		let fileManager = FileManager.default
+		
+		guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+		
+		return documents.appendingPathComponent("items.plist")
+	}
+	
+	init() {
+		loadFromPersistentStore()
+	}
+	
+	func saveToPersistentStore() {
+		guard let url = persistentFileURL else { return }
+		
+		do {
+			let encoder = PropertyListEncoder()
+			let data = try encoder.encode(items)
+			try data.write(to: url)
+		} catch {
+			print("Error saving shopping items: \(error)")
+		}
+	}
+	
+	func loadFromPersistentStore() {
+		let fileManager = FileManager.default
+		guard let url = persistentFileURL, fileManager.fileExists(atPath: url.path) else { return }
+		
+		do {
+			let data = try Data(contentsOf: url)
+			let decoder = PropertyListDecoder()
+			items = try decoder.decode([ShoppingItem].self, from: data)
+		} catch {
+			print("Error loading shopping items: \(error)")
+		}
+	}
+}
---
+++ Shopping List/Model/ShoppingItems.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItems.swift
+//  Shopping List
+//
+//  Created by Marlon Raskin on 6/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem: Codable {
+	var itemName: String
+	var hasBeenAdded: Bool
+}
