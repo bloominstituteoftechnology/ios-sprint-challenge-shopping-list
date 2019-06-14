dee91b2a9c3052b1d6a8c4b4b642e65e7b3caeb5
Jake Connerly <jlconnerly@gmail.com>
2019-06-14 10:16:08 -0500

    Initial Commit

---
+++ Shopping List/View/GroceryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GroceryCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Jake Connerly on 6/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class GroceryCollectionViewCell: UICollectionViewCell {
+    
+}
