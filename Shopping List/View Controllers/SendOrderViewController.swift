65f134fff49b7d0c8d2473a8545767cad9bba8c8
shillwil <alex.shillingford@icloud.com>
2019-07-25 15:40:05 -0600

    MVP functionality achieved

--- Shopping List/View Controllers/SendOrderViewController.swift
+++ Shopping List/View Controllers/SendOrderViewController.swift
@@ -11,19 +11,37 @@ import UIKit
 class SendOrderViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
-    
+    @IBOutlet weak var itemNumberLabel: UILabel!
     @IBOutlet weak var addressTextField: UITextField!
+    
+    var addedItems: [ShoppingItem]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        guard let addedItems = addedItems else { return }
+        
+        if addedItems.count > 1 {
+            itemNumberLabel?.text = "There are \(addedItems.count) items in your shopping list."
+        } else {
+            itemNumberLabel?.text = "There is \(addedItems.count) item in your shopping list."
+        }
     }
     
     @IBAction func sendOrderButtonTapped(_ sender: UIButton) {
-        
+        showAlert()
     }
     
+    func showAlert() {
+        if let name = nameTextField.text,
+            let address = addressTextField.text {
+            if !name.isEmpty && !address.isEmpty {
+                let alert = UIAlertController(title: "Delivery for \(name)!", message: "Your shopping items will be delivered to \(address) in 15 minutes!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+            }
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
--- Shopping List/View Controllers/ShoppingListCollectionViewController.swift
+++ Shopping List/View Controllers/ShoppingListCollectionViewController.swift
@@ -16,24 +16,23 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "NewOrderSegue" {
+            guard let sendOrderVC = segue.destination as? SendOrderViewController else { return }
+            let addedItems: [ShoppingItem] = shoppingItemController.shoppingList.filter({ $0.isAdded == true })
+            
+            sendOrderVC.addedItems = addedItems
+        }
     }
-    */
+    
 
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -58,35 +57,6 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         shoppingItemController.shoppingList[indexPath.item].isAdded.toggle()
         collectionView.reloadData()
