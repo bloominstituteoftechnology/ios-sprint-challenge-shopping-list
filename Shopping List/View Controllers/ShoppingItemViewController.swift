8c44f98f9ee076d978f389173290a9fb0562d4bb
Taylor Lyles <hawks93187@gmail.com>
2019-07-26 10:33:32 -0700

    Added updateLabel() and alert

--- Shopping List/View Controllers/ShoppingItemViewController.swift
+++ Shopping List/View Controllers/ShoppingItemViewController.swift
@@ -21,11 +21,37 @@ class ShoppingItemViewController: UIViewController {
 		
 	}
 	
- 
 	@IBAction func sendOrder(_ sender: UIButton) {
 		guard let name = usersName.text, !name.isEmpty,
 		let address = usersAddress.text, !address.isEmpty
 		else { return }
 	}
+	
+	func updateLabel() {
+		guard let number = shoppingItemController?.wasAdded.count, isViewLoaded else { return }
+		var text = ""
+		switch number {
+		case 0:
+			text = "You currently have \(number) items in your shopping cart"
+		case 1:
+			text = "You currently have \(number) item in your shopping cart"
+		case 2...10:
+			text = "You currently have \(number) items in your shopping cart"
+		default:
+			text = "Error, cannot determine how many things you have in your shopping cart."
+		}
+		numberOfItemsAdded.text = text
+	}
+	
+	
+	func alert(userName: String, userAddress: String){
+		let alert = UIAlertController(title: "Alert message", message: "\(userName), your order will be delivered in 15 minutes to your address \(userAddress)", preferredStyle: .alert)
+		
+		alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+			self.navigationController?.popToRootViewController(animated: true) }))
+		
+		present(alert, animated: true, completion: nil)
+	}
+	
 	
 }
