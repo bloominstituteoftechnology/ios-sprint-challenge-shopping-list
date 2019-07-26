20d31ba53f6b93dc8dee867e8f49ee1007d6fe9a
Taylor Lyles <hawks93187@gmail.com>
2019-07-26 08:47:50 -0700

    Added loadFromPersistentStore()

--- Shopping List/Mode lController/ShoppingListController.swift
+++ Shopping List/Mode lController/ShoppingListController.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class ShoppingListController {
 	
-	let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+	var items = [Item]()
 	
 	//TODO: - Check if this is right
 	init(hasBeenAdded: Bool = true, userDefaults: UserDefaults) {
@@ -28,10 +28,24 @@ class ShoppingListController {
 		guard let url = persistentURL else { return }
 		
 		do {
-			let data = try encoder.encode(itemNames)
+			let data = try encoder.encode(items)
 			try data.write(to: url)
 		} catch {
 			print("Error saving shopping item to persistent store: \(error.localizedDescription)")
+		}
+	}
+	
+	func loadFromPersistentStore() {
+		let decoder = PropertyListDecoder()
+		let fileManager = FileManager.default
+		
+		guard let url = persistentURL, fileManager.fileExists(atPath: url.path) else { return }
+		
+		do {
+			let data = try Data(contentsOf: url)
+			items = try decoder.decode([Item].self, from: data)
+		} catch {
+			print("Error loading shopping items from the persistent store: \(error.localizedDescription)")
 		}
 	}
 
