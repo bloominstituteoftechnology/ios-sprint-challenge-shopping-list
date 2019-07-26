aec34817af6a19b9061c2e82f20b4fa4ca4db409
ry <xxsnailyxx@gmai.com>
2019-07-26 11:05:55 -0700

    updated story board

---
+++ OrderInfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  OrderInfoViewController.swift
+//  Shopping List
+//
+//  Created by Ry Bautista on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class OrderInfoViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
