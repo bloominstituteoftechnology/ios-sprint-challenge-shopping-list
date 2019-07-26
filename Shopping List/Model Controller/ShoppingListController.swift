eea27b4b4e0d530d16c2bf9ec090e29d686591b6
Andrew Ruiz <nukedoom@gmail.com>
2019-07-26 10:38:18 -0700

    Added view controllers and models.

---
+++ Shopping List/Model Controller/ShoppingListController.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Andrew Ruiz on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+    
+    let foo = [
+        ShoppingItem(shoppingItem: "Apple", itemAdded: false, imageName: "Apple"),
+        ShoppingItem(shoppingItem: "Grapes", itemAdded: false, imageName: "Grapes"),
+        ShoppingItem(shoppingItem: "Milk", itemAdded: false, imageName: "Milk"),
+        ShoppingItem(shoppingItem: "Muffin", itemAdded: false, imageName: "Muffin"),
+        ShoppingItem(shoppingItem: "Popcorn", itemAdded: false, imageName: "Popcorn"),
+        ShoppingItem(shoppingItem: "Soda", itemAdded: false, imageName: "Soda"),
+        ShoppingItem(shoppingItem: "Strawberries", itemAdded: false, imageName: "Strawberries")
+        ]
+}
---
+++ Shopping List/View Controllers/ShoppingListDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ShoppingListDetailViewController.swift
+//  Shopping List
+//
+//  Created by Andrew Ruiz on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListDetailViewController: UIViewController {
+
+    @IBOutlet weak var shoppingListStatus: UILabel!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userAddress: UITextField!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    @IBAction func sendOrderButtonTapped(_ sender: Any) {
+    }
+    
+    
+
+}
