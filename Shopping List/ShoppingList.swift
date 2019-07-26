b71c1f5822c2f38e7a2234cad9ce149f04ed77c0
Taylor Lyles <hawks93187@gmail.com>
2019-07-26 08:22:51 -0700

    Added array of item names and Model

---
+++ Shopping List/ShoppingList.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Shopping List
+//
+//  Created by Taylor Lyles on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem: Equatable {
+	
+	var item: String = "Apple"
+	var hasBeenAdded: Bool
+	
+}
