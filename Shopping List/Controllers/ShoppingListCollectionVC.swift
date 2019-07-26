eb74953e17c871fb9caed81c3fbb1f3e1b794355
Jeffrey Santana <SantanaJny@gmail.com>
2019-07-26 13:50:19 -0400

    Creating order screen w/ alert

--- Shopping List/Controllers/ShoppingListCollectionVC.swift
+++ Shopping List/Controllers/ShoppingListCollectionVC.swift
@@ -17,15 +17,14 @@ class ShoppingListCollectionVC: UICollectionViewController {
 
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+		if let orderVC = segue.destination as? OrderVC {
+			orderVC.shoppingListController = shoppingListController
+		}
     }
-    */
 	
 	//MARK: - Methods
 	
