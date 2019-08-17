4026e5c0a9a4d3d1933f549c57b397a0cd936adf
Robert Keffury <bobby.keffury@me.com>
2019-08-17 09:33:31 -0700

    Finished most of the storyboard and set up a few controllers and a model.

---
+++ Shopping List/Model Controller/ShoppingList.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingList.swift
+//  Shopping List
+//
+//  Created by Bobby Keffury on 8/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingList {
+    
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+}
---
+++ Shopping List/Views/GroceryItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GroceryItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Bobby Keffury on 8/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItemCollectionViewCell: UICollectionViewCell {
+    
+}
