b5f3b60bf5470d7a656bcb1d9d9db70feb752797
Joel Groomer <joelgroomer@gmail.com>
2019-08-17 13:20:04 -0700

    MVP achieved. Extra features added - section headers for items in and out of list, coloration of items that are in the list (visually indicates the boolean value for whether they are in the list or not), complete set of address fields on the order page, order page saves name and address so user doesn't have to enter it again, item list on order screen, order not allowed if there are no items or name and address aren't complete.

--- Shopping List/Views/PlaceYourOrderViewController.swift
+++ Shopping List/Views/PlaceYourOrderViewController.swift
@@ -10,10 +10,98 @@ import UIKit
 
 class PlaceYourOrderViewController: UIViewController {
 
+    @IBOutlet weak var lblYouHaveItems: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtAddr1: UITextField!
+    @IBOutlet weak var txtAddr2: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var btnSendOrder: UIButton!
+    @IBOutlet weak var txtvListOfItems: UITextView!
+    
+    var items: [ShoppingItem]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let items = items else { return }
+        
+        lblYouHaveItems.text = "You have \(items.count) item\(items.count != 1 ? "s" : "") in your shopping list."
+        var orderedItems = ""
+        if items.count > 0 {
+            for item in items {
+                orderedItems += item.name + "\n"
+            }
+        } else {
+            orderedItems = "You haven't added any items to your list! Go back and add some now."
+        }
+        txtvListOfItems.text = orderedItems
+        
+        txtName.delegate = self
+        txtAddr1.delegate = self
+        txtAddr2.delegate = self
+        txtCity.delegate = self
+        txtState.delegate = self
+        txtZip.delegate = self
+        
+        txtName.text = UserDefaults.standard.string(forKey: "shoppingName")
+        txtAddr1.text = UserDefaults.standard.string(forKey: "shoppingAddr1")
+        txtAddr2.text = UserDefaults.standard.string(forKey: "shoppingAddr2")
+        txtCity.text = UserDefaults.standard.string(forKey: "shoppingCity")
+        txtState.text = UserDefaults.standard.string(forKey: "shoppingState")
+        txtZip.text = UserDefaults.standard.string(forKey: "shoppingZip")
     }
 
+    @IBAction func sendOrderTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Your order has been placed", message: "Thanks for your order, \(txtName.text ?? "")! You can expect delivery in just 15 minutes to \(txtAddr1.text ?? "").", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+}
+
+extension PlaceYourOrderViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if !txtName.text!.isEmpty &&
+            !txtAddr1.text!.isEmpty &&
+            // Addr 2 isn't required
+            !txtCity.text!.isEmpty &&
+            !txtState.text!.isEmpty &&
+            !txtZip.text!.isEmpty
+        {
+            UserDefaults.standard.set(txtName.text, forKey: "shoppingName")
+            UserDefaults.standard.set(txtAddr1.text, forKey: "shoppingAddr1")
+            UserDefaults.standard.set(txtAddr2.text, forKey: "shoppingAddr2")
+            UserDefaults.standard.set(txtCity.text, forKey: "shoppingCity")
+            UserDefaults.standard.set(txtState.text, forKey: "shoppingState")
+            UserDefaults.standard.set(txtZip.text, forKey: "shoppingZip")
+            
+            if let items = items, items.count > 0 {
+                btnSendOrder.isEnabled = true
+            }
+        } else {
+            btnSendOrder.isEnabled = false
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text, !text.isEmpty {
+            switch textField {
+            case txtName:
+                txtAddr1.becomeFirstResponder()
+            case txtAddr1:
+                txtAddr2.becomeFirstResponder()
+            case txtAddr2:
+                txtCity.becomeFirstResponder()
+            case txtCity:
+                txtState.becomeFirstResponder()
+            case txtState:
+                txtZip.becomeFirstResponder()
+            default:
+                textField.resignFirstResponder()
+            }
+        }
+        return false
+    }
 }
--- Shopping List/Views/ShoppingListCollectionViewController.swift
+++ Shopping List/Views/ShoppingListCollectionViewController.swift
@@ -26,16 +26,19 @@ class ShoppingListCollectionViewController: UICollectionViewController {
         // Do any additional setup after loading the view.
     }
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "segueShowPlaceOrder" {
+            if let vc = segue.destination as? PlaceYourOrderViewController {
+                vc.items = shoppingList.inList
+            }
+        }
     }
-    */
 
+    
+    
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
