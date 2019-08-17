59dc22bfb79efbd19a34e2324637f1cdbc5ef43f
Joel Groomer <joelgroomer@gmail.com>
2019-08-17 11:56:01 -0700

    Order screen created

---
+++ Shopping List/Views/PlaceYourOrderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlaceYourOrderViewController.swift
+//  Shopping List
+//
+//  Created by Joel Groomer on 8/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PlaceYourOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
