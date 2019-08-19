ba74e50f84a2390db1583de5fe264f85cf629441
Aaron Haughton <AaronJHaughton@outlook.com>
2019-08-18 21:10:51 -0400

    initial commit. added model

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Aaron on 8/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    
+    let name: String
+    let addedToList: Bool
+    
+}
