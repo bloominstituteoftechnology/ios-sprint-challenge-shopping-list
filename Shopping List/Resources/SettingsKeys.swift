98eefcb0e7c562c8fa590a350876d4cd172538fc
Eoin Lavery <dev@tenantc.co.uk>
2019-08-19 03:41:29 +0100

    Added ViewController Storyboard for CollectionView Added Customer UICollectionViewCell Files Added ShoppingItem Model and Shopping List Model Controller

---
+++ Shopping List/Resources/SettingsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsKeys.swift
+//  Shopping List
+//
+//  Created by Eoin Lavery on 19/08/2019.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static var shoppingListKey = "shoppingListKey"
+}
