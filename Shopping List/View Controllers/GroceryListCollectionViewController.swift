38a4a5f9066d35fd8c6d5139b86306931830401b
John Kouris <jdkouris@gmail.com>
2019-08-21 13:15:58 -0700

    Removed an unnecessary comment

--- Shopping List/View Controllers/GroceryListCollectionViewController.swift
+++ Shopping List/View Controllers/GroceryListCollectionViewController.swift
@@ -24,7 +24,6 @@ class GroceryListCollectionViewController: UICollectionViewController {
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowOrderSegue" {
             guard let destinationVC = segue.destination as? ShoppingListDetailViewController else { return }
