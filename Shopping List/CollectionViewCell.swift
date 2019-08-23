a18418b5ec920176059079a3e646129626dc2ec1
William Chen <chen.will7@gmail.com>
2019-08-23 08:38:12 -0700

    all controllers wired up!

---
+++ Shopping List/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  Shopping List
+//
+//  Created by William Chen on 8/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
