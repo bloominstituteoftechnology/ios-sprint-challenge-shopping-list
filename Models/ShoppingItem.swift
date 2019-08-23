5bad4293460fdb58fe791dbe89b89993e0838db5
Percy <percival.ngan@gmail.com>
2019-08-23 11:09:30 -0500

    Added view scenes in storyboard

---
+++ Models/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Percy Ngan on 8/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+	var name: String
+	var addedToList: Bool
+	var image: Data
+}
