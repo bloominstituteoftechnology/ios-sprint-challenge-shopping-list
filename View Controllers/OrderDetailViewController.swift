b2d39a868fe3e1c199e6509f2caf7c9a9ba07495
Percy <percival.ngan@gmail.com>
2019-08-23 12:34:18 -0500

    added Alert in order form

--- View Controllers/OrderDetailViewController.swift
+++ View Controllers/OrderDetailViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class OrderDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
+
+	@IBOutlet weak var orderSummaryLabel: UILabel!
+	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var addressTextField: UITextField!
+
+
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+
+	@IBAction func orderButtonTapped(_ sender: UIButton) {
+		showAlert()
+		nameTextField.text = nil
+		addressTextField.text = nil
+	}
+	
+	
+	// MARK: - METHODS
+
+	func showAlert() {
+		guard let name = nameTextField.text else { return }
+		guard let address = addressTextField.text else { return }
+		let alert = UIAlertController(title: "Delivery for \(name)!", message: "Your shopping items will be delivered to \(address) in 15 minutes!", preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+		present(alert, animated: true, completion: nil)
+	}
+
+
+	// MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
