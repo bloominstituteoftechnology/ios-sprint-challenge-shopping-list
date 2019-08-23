33ab11582d493d9b8ae50b14ba63d71c1b3899a3
Jerry Haaser <jerry.haaser@hotmail.com>
2019-08-23 12:05:06 -0500

    set up sendOrderViewController

--- Shopping List/ViewControllers/SendOrderViewController.swift
+++ Shopping List/ViewControllers/SendOrderViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 
 class SendOrderViewController: UIViewController {
     
+    
+    @IBOutlet weak var sendOrderLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     
-
+    var shoppingItem : ShoppingItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        sendOrderLabel.text = "You currently have \(shoppingItem) items in your shopping list."
+        
     }
     
     @IBAction func sendOrderButton(_ sender: UIButton) {
         
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "Delivery for \(String(describing: nameTextField.text))", message: "Your shopping items will be delivered to \(String(describing: addressTextField.text))", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     
