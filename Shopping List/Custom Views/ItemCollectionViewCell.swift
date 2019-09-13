fd00161f6f39978f42656306e20d3bf875ebef2d
ladybeitel <ciarabeitel@gmail.com>
2019-09-13 18:46:20 -0400

    Create project. Setup storyboard with scenes, that include labels, buttons, text fields, nav bars, etc. Constrain elements. Create show segue on nav bar to detailVC.

---
+++ Shopping List/Custom Views/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Ciara Beitel on 9/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Ciara Beitel on 9/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    var name: String
+    var addedToList: Bool
+}
