bf09b9d112a45da6d012c5a5683e253d61786570
Wilma <kima_92@fastmail.com>
2019-09-20 13:03:23 -0400

    Wired up Storyboard

---
+++ View Controllers/ShoppingListDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ShoppingListDetailViewController.swift
+//  Shopping List
+//
+//  Created by macbook on 9/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListDetailViewController: UIViewController {
+    
+    
+    // MARK : - IBOutlets
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    
+    
+    
+    // VIEW DID LOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    
+    // MARK: - IBAction Submit Button
+    @IBAction func submitOrderButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Views/ShoppingListCollectionViewCell.swift
+++ Views/ShoppingListCollectionViewCell.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class ShoppingListCollectionViewCell: UICollectionViewCell {
     
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var isAddedLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    var shoppingItem: ShoppingItem? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        
+        guard let shoppingItem = shoppingItem else { return }
+        
+        
+//        imageView.image = planet.image
+//        textLabel.text = planet.name
+        
+    }
+    
 }
