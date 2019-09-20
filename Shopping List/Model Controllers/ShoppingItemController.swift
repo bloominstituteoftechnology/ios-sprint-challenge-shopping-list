7e784428d76ddf5af7620b729b491d8c8b30e05a
Gi Pyo Kim <gipyok@gmail.com>
2019-09-20 08:54:13 -0700

    Basic setup of the project: created custom classes and setup UI

---
+++ Shopping List/Model Controllers/ShoppingItemController.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Gi Pyo Kim on 9/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Shopping List/Views/ShoppingListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Gi Pyo Kim on 9/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewCell: UICollectionViewCell {
+    
+}
