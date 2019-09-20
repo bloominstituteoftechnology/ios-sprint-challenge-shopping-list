85845407474c5dcf187142f8c6e15faa60fc9e84
Jerry Haaser <jerry.haaser@hotmail.com>
2019-09-20 11:22:51 -0500

    set up model and model controller

---
+++ Shopping List/ModelController/ShoppingItemController.swift
@@ -0,0 +1,68 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by admin on 9/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingItemController {
+    
+    var items: [ShoppingItem] = []
+    
+    init() {
+        createItem()
+        loadFromPersistenceStore()
+    }
+    
+    func saveToPersistenceStore() {
+        guard let url = shoppingListURL else { return }
+        do {
+            let encoder = PropertyListEncoder()
+            let itemData = try encoder.encode(items)
+            try itemData.write(to: url)
+        } catch {
+            print("Error loading item data: \(error)")
+        }
+    }
+    
+    func createItem() {
+        guard UserDefaults.standard.bool(forKey: "ItemsHaveBeenCreated") != true else { return }
+        
+        let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+        
+        for name in itemNames {
+            let shoppingItem = ShoppingItem(itemName: name)
+            items.append(shoppingItem)
+        }
+        
+        saveToPersistenceStore()
+    }
+    
+    func updateHasSeen(forShoppingItem shoppingItem: ShoppingItem) {
+        shoppingItem.added = !shoppingItem.added
+        saveToPersistenceStore()
+    }
+    
+    func loadFromPersistenceStore() {
+        do{
+            guard let url = shoppingListURL else { return }
+            let data = try Data(contentsOf: url)
+            let decodeItems = PropertyListDecoder()
+            items = try decodeItems.decode([ShoppingItem].self, from: data)
+        } catch {
+            print("Error loading item data: \(error)")
+        }
+    }
+    
+    private var shoppingListURL: URL? {
+        let fm = FileManager.default
+        guard let dir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return dir.appendingPathComponent("ShoppingList.plist")
+        
+    }
+    
+}
--- Shopping List/ShoppingItemController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ShoppingItemController.swift
-//  Shopping List
-//
-//  Created by admin on 9/20/19.
-//  Copyright © 2019 Lambda School. All rights reserved.
-//
-
-import Foundation
