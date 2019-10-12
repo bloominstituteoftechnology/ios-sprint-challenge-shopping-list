de90e294bdd65839e80556cb6d84e095be75d9a7
Gavin Murphy <gavincmurphy@gmail.com>
2019-10-11 22:04:27 -0700

    Added some UI to main storyboard and settings storyboard

--- Shopping List/Model/ShoppingItem.swift
+++ Shopping List/Model/ShoppingItem.swift
@@ -10,14 +10,5 @@ import Foundation
 
 struct ShoppingItem {
     
-    var name: String
-    var image: UIImage
-    var itemAdded: Bool = false
-    
-    init(name: String, imageName: String, itemAdded: Bool) {
-        self.name = name
-        self.image = UIImage(named: imageName)!
-        self.itemAdded = itemAdded
-    }
 }
 
