4972f5aa8351f2a9f1afb5ae18751349f328bb7b
Joseph Rogers <joerogersss2016@gmail.com>
2019-10-11 20:24:54 -0500

    Model Set up-Storyboards finished

---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,16 @@
+//
+// ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Joseph Rogers on 10/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+ let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+
+struct ShoppingItem {
+    var name: String
+    var addedToList: Bool = false
+}
