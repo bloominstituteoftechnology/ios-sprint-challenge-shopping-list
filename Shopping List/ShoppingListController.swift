929a5f23b7bf3b50ae884d4780628835c97f613e
Christian Lorenzo <clbmiami2004@gmail.com>
2019-10-12 09:35:10 -0700

    Created the Array

---
+++ Shopping List/ShoppingListController.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Lambda_School_Loaner_201 on 10/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"] 
+}
