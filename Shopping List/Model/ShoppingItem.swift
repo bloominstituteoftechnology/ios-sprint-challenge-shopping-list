a898cdd11e51dc3ca2d71b6e18ed374a37c91e55
Gavin Murphy <gavincmurphy@gmail.com>
2019-10-11 19:10:38 -0700

    Added model ShoppingItem with a struct

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Gavin Murphy on 10/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    var name: String
+    var itemAdded: Bool = false
+}
