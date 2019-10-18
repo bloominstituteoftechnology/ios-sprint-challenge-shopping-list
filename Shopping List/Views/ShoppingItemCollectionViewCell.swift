d25bb18e500f1271c71a66450dfa70a2259c49e9
Jon Bash <jonbashmusic@gmail.com>
2019-10-18 09:03:32 -0700

    add cvcell file

---
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Jon Bash on 2019-10-18.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
