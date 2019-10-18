18491c8dcca54de52e2e668d14215d605676eaf5
Rwoltx <richardx1016@gmail.com>
2019-10-18 13:08:25 -0400

    ShoppingItemCollectionViewCell completed and connected

---
+++ Shopping List/View Controllers/CartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CartViewController.swift
+//  Shopping List
+//
+//  Created by Rick Wolter on 10/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
