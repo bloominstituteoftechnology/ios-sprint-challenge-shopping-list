3b926a5d72edbe94c492fdaf17fe11c2135b404f
Brandi <blubrandi0@gmail.com>
2019-10-18 11:38:38 -0600

    Added outlets for itemCount, nameLabel, and addressLabel, and action for submitOrderButton to created OrderViewController file.  Also organized files into groups.

---
+++ Shopping List/View Controllers/OrderViewController.swift
@@ -0,0 +1,39 @@
+//
+//  OrderViewController.swift
+//  Shopping List
+//
+//  Created by Brandi on 10/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    @IBOutlet weak var itemCount: UILabel!
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var addressLabel: UITextField!
+    
+    
+    @IBAction func submitOrderButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
