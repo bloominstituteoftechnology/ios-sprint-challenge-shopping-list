57f31ecfe99f6dce352f2f0173aba58d6d674244
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-18 10:37:44 -0700

    Alert for submitting order MVP. Sprint 3 MVP completion

--- Shopping List/ViewControllers/SendOrderViewController.swift
+++ Shopping List/ViewControllers/SendOrderViewController.swift
@@ -17,6 +17,7 @@ class SendOrderViewController: UIViewController {
     
     var shoppingListController: ShoppingListController?
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,18 +26,31 @@ class SendOrderViewController: UIViewController {
     }
     
 
+    // MARK: - Class Functions
     func updateViews() {
         guard let shoppingListController = shoppingListController else { return }
         
+        nameTextField.becomeFirstResponder()
         let shoppingListCount = shoppingListController.addedItems.count
         
         titleLabel.text = "You currently have \(shoppingListCount) item(s) in your shopping list."
+    }
+    
+    private func showOrderAlert() {
+        let alert = UIAlertController(title: "Order Submitted!", message: "\(nameTextField.text!) your order will be delivered in 15 minutes to \(addressTextField.text!).", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
     // MARK: - IBActions
     
     @IBAction func sendOrderTapped(_ sender: UIButton) {
+        guard let nameText = nameTextField.text,
+            let addressText = addressTextField.text,
+            !nameText.isEmpty,
+            !addressText.isEmpty else { return }
         
+        showOrderAlert()
     }
 
 }
