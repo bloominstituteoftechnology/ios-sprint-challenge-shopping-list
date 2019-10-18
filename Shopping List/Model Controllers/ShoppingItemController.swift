ca56075072a4887a76e23faeb8bf72215fe6337d
Jon Bash <jonbashmusic@gmail.com>
2019-10-18 08:22:21 -0700

    implement shoppingitemcontroller userdefaults

---
+++ Shopping List/Model Controllers/ShoppingItemController.swift
@@ -0,0 +1,41 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Jon Bash on 2019-10-18.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingItemController {
+    // MARK: - Settings
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+    var itemsListInitialized: Bool {
+        // returns false if key not found
+        return UserDefaults.standard.bool(forKey: .itemsListInitializedKey)
+    }
+    
+    // MARK: - Properties
+    
+    var items: [ShoppingItem]?
+    
+    init() {
+        if !itemsListInitialized {
+            initializeItemsList()
+        }
+    }
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+    
+    private func initializeItemsList() {
+        
+        UserDefaults.standard.set(true, forKey: .itemsListInitializedKey)
+    }
+}
+
+extension String {
+    static let itemsListInitializedKey = "ItemsListInitialized"
+}
