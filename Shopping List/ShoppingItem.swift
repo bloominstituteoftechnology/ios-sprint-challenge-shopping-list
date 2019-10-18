1102085e0978d421722256cf5d6558a52f1af4c9
Rwoltx <richardx1016@gmail.com>
2019-10-18 12:24:01 -0400

    Finished UI

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Rick Wolter on 10/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem{
+    
+    
+    let name: String
+    var isInList: Bool
+    
+    
+    
+    
+}
---
+++ Shopping List/ShoppingItemController.swift
@@ -0,0 +1,12 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Rick Wolter on 10/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+
