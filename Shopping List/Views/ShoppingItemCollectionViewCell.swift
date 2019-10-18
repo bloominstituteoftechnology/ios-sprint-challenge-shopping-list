5b3ded8efd2a8fcec81e5f5fd3e3bb020944ddf6
Dan M <djmthedev@gmail.com>
2019-10-18 08:47:11 -0700

    implemented itemCell-still need to add functionality to change the label

--- Shopping List/Views/ShoppingItemCollectionViewCell.swift
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var addedLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var shoppingItem: ShoppingItem? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let shoppingItem = shoppingItem, let image = UIImage(named: shoppingItem.name) else { return }
+        
+        var addedText = ""
+        if shoppingItem.added {
+            addedText = "Added"
+        } else {
+            addedText = "Not Added"
+        }
+        
+        addedLabel.text = addedText
+        imageView.image = image
+        nameLabel.text = shoppingItem.name
+    }
+    
 }
