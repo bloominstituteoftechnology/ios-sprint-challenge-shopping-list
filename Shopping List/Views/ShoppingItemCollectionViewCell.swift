5316d495dfd6925d0b7d73d3be2507dd5ba0efbc
Jon Bash <jonbashmusic@gmail.com>
2019-10-18 09:32:43 -0700

    update views on cell item didset

--- Shopping List/Views/ShoppingItemCollectionViewCell.swift
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
-    var shoppingItem: ShoppingItem?
+    var shoppingItem: ShoppingItem? {
+        didSet {
+            updateViews()
+        }
+    }
     
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
