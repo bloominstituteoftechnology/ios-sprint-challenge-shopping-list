28f3f09a43245d574742f99b116728bf2eb5e467
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2019-11-04 20:09:09 -0500

    MV?P

---
+++ Shopping List/SendOrderDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SendOrderDetailViewController.swift
+//  Shopping List
+//
+//  Created by Thomas Sabino-Benowitz on 10/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SendOrderDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
