8014e1edb0613813f6a2ba961e5752bcf60386fa
Rohan Taylor <rhntaylor@gmail.com>
2019-11-15 11:01:17 -0800

    notDone

---
+++ Shopping List/Resources/shoppinglistcontroller.swift
@@ -0,0 +1,34 @@
+//
+//  shoppinglistcontroller.swift
+//  Shopping List
+//
+//  Created by Rohan Taylor on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+class ShoppingListController {
+    
+    var shoppingItems = [ShoppingItem] {
+    
+    let itemNames = [ShoppingItem(item: "Apple", addedToList: Bool?),
+                     [ShoppingItem(item: "Soda", addedToList: Bool),
+                      [ShoppingItem(item: "Grapes", addedToList: Bool?),
+                       [ShoppingItem(item: "Milk", addedToList: Bool?),
+                        [ShoppingItem(item: "Muffin", addedToList: Bool?),
+                         [ShoppingItem(item: "Popcorn", addedToList: Bool?),
+                          [ShoppingItem(item: "Strawberries", addedToList: Bool?)]
+        
+        
+    
+    
+         let shouldShowItem = UserDefaults.standard.bool(forKey: itemNames)
+            
+            if shouldShowPluto {
+                result.append(Planet(name: "Pluto"))
+            }
+            
+            return result
+        }
---
+++ Shopping List/Resources/shoppinglistpropsetup.swift
@@ -0,0 +1,15 @@
+//
+//  shoppinglistpropsetup.swift
+//  Shopping List
+//
+//  Created by Rohan Taylor on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct ShoppingItem {
+    let item : String
+    let addedToList : Bool?
+}
