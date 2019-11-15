f5f0a9c87375afbf2ea4a1f5a0f40dc90da7166a
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-15 08:19:10 -0800

    Initial commit. Added model and cleaned up storyboard.

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Donella Barcelo on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    let name: String
+    let addedToList: Bool
+}
