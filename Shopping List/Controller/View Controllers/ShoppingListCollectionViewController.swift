6dedfa095b24980865295c56fbaa75c6aced7fa9
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-15 11:26:49 -0500

    Added 'Constants' 'ShoppingListCollectionVC' and set up storyboard (UI)

---
+++ Shopping List/Controller/View Controllers/ShoppingListCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingListCollectionViewController.swift
+//  Shopping List
+//
+//  Created by Chad Rutherford on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewController: UICollectionViewController {
+    
+}
---
+++ Shopping List/Supporting Files/Resources/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Shopping List
+//
+//  Created by Chad Rutherford on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Cells {
+    static let itemCell = "ItemCell"
+}
