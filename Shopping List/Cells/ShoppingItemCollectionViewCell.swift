0619d58e2b9bf82d3e54e6194736794d7200339f
Waseem <idelbiw@gmail.com>
2019-12-15 22:57:54 -0500

    got all the basics in the storyboard down

---
+++ Shopping List/Cells/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Waseem Idelbi on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
---
+++ Shopping List/Models/Shopping Item.swift
@@ -0,0 +1,15 @@
+//
+//  Shopping Item.swift
+//  Shopping List
+//
+//  Created by Waseem Idelbi on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Item {
+    let name: String
+    let itemWasAdded: Bool
+}
