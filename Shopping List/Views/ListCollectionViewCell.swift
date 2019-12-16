c9bb3f6ef6048cc6ade71d0d56740cf059e2f403
David Williams <burmesedragon@att.net>
2019-12-15 19:12:37 -0800

    more storyboard work

---
+++ Shopping List/Views/ListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by David Williams on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ListCollectionViewCell: UICollectionViewCell {
+    
+}
