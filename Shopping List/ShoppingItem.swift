2b36a89fc2260f23f188262ee6e4ad4118868b2f
Gerardo Hernandez <gahernandez02@gmail.com>
2019-12-15 18:21:15 -0800

    Initital commit. Created model.

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Gerardo Hernandez on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    let name = "Apple"
+    let hasBeenAdded = false 
+}
