73bc9d0ff05f4471acf7c7d9cde537e2b26360a8
John Holowesko <john@sipsip.io>
2019-12-15 21:52:00 -0500

    IBOutlets & IBActions connected

--- Shopping List/View Controllers/ItemCollectionViewCell.swift
+++ Shopping List/View Controllers/ItemCollectionViewCell.swift
@@ -2,4 +2,9 @@ import UIKit
 
 class ItemCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - IBOutlets
+    @IBOutlet var itemAddedBool: UILabel!
+    @IBOutlet var itemImage: UIImageView!
+    @IBOutlet var itemName: UILabel!
+    
 }
