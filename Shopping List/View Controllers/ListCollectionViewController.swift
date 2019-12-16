47c06ea84443f70ce8502e47ac0e86f03fc14842
David Williams <burmesedragon@att.net>
2019-12-15 21:04:35 -0800

    not finished.. having issues with changing the button image and cells showing up too large, for starters

--- Shopping List/View Controllers/ListCollectionViewController.swift
+++ Shopping List/View Controllers/ListCollectionViewController.swift
@@ -10,38 +10,30 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
+protocol ShoppingListDelegate {
+    func shouldBeAdded()
+}
+
 class ListCollectionViewController: UICollectionViewController {
-   
+    
  let shoppingController = ShoppingController()
     
+    var notAdded: Bool = true
+    
+    var delegate: ShoppingListDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
 
-   
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     // MARK: UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
+        
         return shoppingController.items.count
     }
 
@@ -56,19 +48,18 @@ class ListCollectionViewController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-   
-   
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
+    func shouldBeAdded() {
+        self.collectionView?.reloadData()
     }
    
-
     @IBAction func addedButtonTapped(_ sender: Any) {
+        notAdded = !notAdded
+        
+        let userDefaults = UserDefaults.standard
+        userDefaults.bool(forKey: .notAdded)
+        delegate?.shouldBeAdded()
     }
+}
+extension String {
+    static var notAdded = "notAdded"
 }
