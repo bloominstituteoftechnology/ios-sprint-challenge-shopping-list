fafb0d975eefd41294496c11ba4fb929dc47c05d
Claudia Maciel <clc_80@yahoo.com>
2020-02-21 20:52:30 -0800

    Changed persistance and added toggle function

--- Shopping List/Controller/ShoppingListController.swift
+++ Shopping List/Controller/ShoppingListController.swift
@@ -8,88 +8,107 @@
 
 import Foundation
 
+
 class ShoppingListController {
-    var shoppingItems: [ShoppingItem] {
-        var shoppingItems = [
-            ShoppingItem(name: "Apple", addedToShoppingList: false),
-            ShoppingItem(name: "Grapes", addedToShoppingList: false),
-            ShoppingItem(name: "Milk", addedToShoppingList: false),
-            ShoppingItem(name: "Muffin", addedToShoppingList: false),
-            ShoppingItem(name: "Popcorn", addedToShoppingList: false),
-            ShoppingItem(name: "Soda", addedToShoppingList: false),
-            ShoppingItem(name: "Strawberries", addedToShoppingList: false)
-        ]
-        
-        return shoppingItems
-    }
+
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
     
-
-    //MARK: - Persistance
-    private var persistentFileURL: URL? {
-        let fileManager = FileManager.default
-        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-        return documents.appendingPathComponent("shoppingList.plist")
-    }
-
-    init() {
-        loadFromPersistentStore()
-    }
-    
-   //MARK: - Functions
-    func updateItem(ShoppingItem: ShoppingItem, addedToShoppinglist: Bool) {
+    var hasData: Bool {
+        let setting = UserDefaults.standard.bool(forKey: .isFirstRunKey)
         
-    }
-
-    //MARK: - Persistance Methods
-    func saveToPersistentStore() {
-        
-        //place to store the data
-        guard let url = persistentFileURL else { return }
-        
-        //Need to convert the data to something that can be stored on the phone
-        do {
-            //Get ready to encode the data
-            let encoder = PropertyListEncoder()
-            
-            //the encoded data
-            let data = try encoder.encode(shoppingItems)
-            
-            //write to the url
-            try data.write(to: url)
-            
-        } catch {
-            print("Error saving data: \(error)")
+        if !setting {
+            return false
         }
         
+        return true
+        
+    }
+    
+    var numberOfItemsAdded: Int {
+        let filtered = shoppingItems.filter { return $0.addedToShoppingList }
+        return filtered.count
+    }
+    
+    init() {
+        if !hasData {
+            for item in itemNames {
+                createShoppingItem(name: item, addedToShoppingList: false)
+            }
+
+            UserDefaults.standard.set(true, forKey: .isFirstRunKey)
+
+        } else {
+          loadFromPersistentStore()
+        }
+
+
+    }
+
+    var shoppingItems: [ShoppingItem] = []
+
+    var persistentFileURL: URL? {
+        let fileManager = FileManager.default
+
+        guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+
+        let shoppingItemURL = documentsDirectory.appendingPathComponent("shoppingItems.plist")
+
+        return shoppingItemURL
+
+    }
+
+    func saveToPersistentStore() {
+        guard let fileURL = persistentFileURL else { return }
+
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(shoppingItems)
+            try data.write(to: fileURL)
+
+
+        } catch {
+            print(error)
+        }
+
     }
 
     func loadFromPersistentStore() {
-        let fileManager = FileManager.default
-        //Make sure that the file exists at our selected path
-        guard let url = persistentFileURL, fileManager.fileExists(atPath: url.path) else { return }
-        
+        guard let fileURL = persistentFileURL else { return }
+
         do {
-            //pull the data from the url
-            let data = try Data(contentsOf: url)
-            
-            //to decode the data
+
             let decoder = PropertyListDecoder()
-            
-            // Decode the data and place in array (we specify what type to decode itself into)
-            //shoppingItems = try decoder.decode([ShoppingItem].self, from: data)
-            
+
+            let propertyListData = try Data(contentsOf: fileURL)
+            let data = try decoder.decode([ShoppingItem].self, from: propertyListData)
+
+            shoppingItems = data
+
+
         } catch {
-            print("error loading data: \(error)")
+            print(error)
         }
-    }
-    
-    func savePreference() {
-        UserDefaults.standard.set(shoppingItems, forKey: itemPreferenceKey)
+
     }
 
-    func loadPerference() {
-        let itemPerference = UserDefaults.standard.string(forKey: itemPreferenceKey)
+    func createShoppingItem(name: String, addedToShoppingList: Bool) {
+        let shoppingItem = ShoppingItem(name: name, addedToShoppingList: addedToShoppingList)
+        shoppingItems.append(shoppingItem)
+        saveToPersistentStore()
     }
 
-    let itemPreferenceKey = "itemPreference"
+    func toggleHasBeenAdded(for item: ShoppingItem) {
+        if let shoppingItemIndex = shoppingItems.firstIndex(of: item) {
+            var shoppingItem = shoppingItems[shoppingItemIndex]
+            shoppingItem.addedToShoppingList.toggle()
+            shoppingItems[shoppingItemIndex] = shoppingItem
+            saveToPersistentStore()
+
+        }
+
+    }
+}
+
+extension String {
+    static let isFirstRunKey = "ShoppingListIsFirstRunKey"
 }
