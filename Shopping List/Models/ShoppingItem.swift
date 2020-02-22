e2f5218632826e873324a3426be2ac40449a1468
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-21 21:44:26 -0500

    Created UI and added class files

---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Lambda_School_Loaner_259 on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem {
+    var itemName: String
+    var hasBeenAdded: Bool
+    var itemImage: UIImage? {
+        return UIImage(contentsOfFile: "\(itemName).png")
+    }
+}
---
+++ Shopping List/View Controllers/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Lambda_School_Loaner_259 on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
