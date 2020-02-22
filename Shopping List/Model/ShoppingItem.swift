2f5d9dd13f6efcf026601f09c9b285f39533c307
Claudia Maciel <clc_80@yahoo.com>
2020-02-21 18:14:02 -0800

    ShoppingItem Model created

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Claudia Contreras on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ShoppingItem {
+    var name: String
+    var addedToShoppingList: Bool
+    
+    var image: UIImage {
+        return UIImage(named: name)!
+    }
+}
