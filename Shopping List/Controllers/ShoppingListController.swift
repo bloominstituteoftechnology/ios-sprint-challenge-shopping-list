caf0f00e68389c086aa846b5b6370e41aa610dad
kendra rayne <raynekendra@gmail.com>
2020-02-22 12:40:18 -0500

    No errors so far! woo

---
+++ Shopping List/Controllers/ShoppingListController.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Kendra McVay on 2/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    var shoppingList: [ShoppingItem] =
+        [ShoppingItem(name: "Apple", addedToList: false),
+         ShoppingItem(name: <#T##String#>, addedToList: <#T##Bool#>)
+                                    ]
+}
+
+//let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Kendra McVay on 2/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItem {
+var name: String
+var addedToList: Bool = false
+
+init(name: String, addedToList: Bool = false) {
+    self.name = name
+    self.addedToList = addedToList
+}
+    }
+    
+
