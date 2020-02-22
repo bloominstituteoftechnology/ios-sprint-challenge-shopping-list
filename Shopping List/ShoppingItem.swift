d80b6f81484b07bacbaac9f723b62db61e4acf66
TrueBallin <ballin_true@yahoo.com>
2020-02-21 18:28:33 -0800

    Set up

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by True Ballin on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingItem {
+    var apple: String
+    var hasBeenAdded: Bool
+    
+    init(apple: String, hasBeenAdded: Bool) {
+        self.apple = apple
+        self.hasBeenAdded = false
+        
+    }
+}
---
+++ Shopping List/ShoppingList.swift
@@ -0,0 +1,12 @@
+//
+//  ShoppingList.swift
+//  Shopping List
+//
+//  Created by True Ballin on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+ let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
