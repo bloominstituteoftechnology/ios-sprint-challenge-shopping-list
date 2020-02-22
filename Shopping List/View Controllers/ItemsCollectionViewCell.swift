35642bddbecc603e2200e656d461d7ce36d316e4
Robert Vance <Fearmydotzs@gmail.com>
2020-02-21 21:39:55 -0700

    Not done yet

---
+++ Shopping List/View Controllers/ItemsCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemsCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Rob Vance on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ItemsCollectionViewCell: UICollectionViewCell {
+
+// Mark IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemAddedLabel: UILabel!
+    
+    
+    
+    
+}
