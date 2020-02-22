b3a3d13c1fe37c16805b970a0238d61eeafc936d
hai666l <0xconr@protonmail.com>
2020-02-21 20:22:57 -0600

    Inital Commit

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by conner on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ShoppingItem {
+    var name: String
+    var added: Bool
+    var image: UIImage {
+        
+    }
+    
+}
