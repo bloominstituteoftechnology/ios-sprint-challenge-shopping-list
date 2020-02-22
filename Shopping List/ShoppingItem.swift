c32f108fd4704b0d153e8c0b8520d129c71a7d9a
kensam1 <samarakodykennedy@gmail.com>
2020-02-21 21:50:43 -0500

    Set up the UI

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Lambda_School_Loaner_241 on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
