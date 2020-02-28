f0c5a7efc13b11aa085fb6d4fc75465bcd9da0a2
Hector L <hectoremil21@hotmail.com>
2020-02-28 13:07:17 -0500

    Create outlets from order view.

--- Shopping List/Controllers/OrderViewController.swift
+++ Shopping List/Controllers/OrderViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class OrderViewController: UIViewController {
 
+    @IBOutlet weak var listSizeLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    
+    var itemController: ShoppingItemController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func orderButton(_ sender: Any) {
     }
-    */
+    
+    
+    
 
 }
