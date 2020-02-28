0ab069479332ce3c6168763fa5987a40459ee6a0
Nichole Davidson <nicholedavid@gmail.com>
2020-02-28 10:12:55 -0700

    ShoppingListController and persistence

--- Model/ShoppingItem.swift
+++ Model/ShoppingItem.swift
@@ -6,20 +6,24 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-struct ShoppingItem {
+class ShoppingItem: Codable {
     
     var name: String
     var itemAdded: Bool
     var imageName: String
     
     var itemImage: UIImage {
-      UIImage(named: imageName)
+        UIImage(named: imageName)!
     }
     
-    init(name: String, itemAdded: Bool){
+    init(name: String, itemAdded: Bool = false){
         self.name = name
         self.itemAdded = itemAdded
+        self.imageName = name.lowercased()
     }
+    
+ 
 }
+
---
+++ Model/ShoppingListController.swift
@@ -0,0 +1,83 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Nichole Davidson on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    var items:[ShoppingItem] = []
+    
+    func createShoppingList() {
+        UserDefaults.standard.bool(forKey: "itemAdded")
+        
+        let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+        
+        for itemName in itemNames {
+            let shoppingItem = ShoppingItem(name: itemName)
+            items.append(shoppingItem)
+        }
+    }
+    
+    // MARK: - Persistence
+    
+    // The location of the Stars file that we will be saving to and loading from
+    var persistentFileURL: URL? {
+        
+        let fileManager = FileManager.default
+        
+        // ~/MyApp/Documents/
+        let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        // ~/MyApp/Documents/stars.plist
+        let itemsURL = documentsDir?.appendingPathComponent("items.plist")
+        
+        return itemsURL
+    }
+    
+    // Saves the stars to the persistent file URL
+    func saveToPersistentStore() {
+        // Convert Stars into a Property List
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+        
+            let itemsPlist = try encoder.encode(items)
+            
+            guard let itemsURL = persistentFileURL else { return }
+            
+           try itemsPlist.write(to: itemsURL)
+            
+        } catch {
+            // The catch statement gets called if the function(s) that you call "try" on fails.
+            print("Unable to save items to plist: \(error)")
+            
+        }
+        
+    }
+    
+    func loadFromPersistentStore() {
+        
+        guard let itemsURL = persistentFileURL else { return }
+        
+        let decoder = PropertyListDecoder()
+        
+        do {
+            // Goes to the starsURL and grab the file (data) from that location
+            let itemsData = try Data(contentsOf: itemsURL)
+            
+            let itemsArray = try decoder.decode([ShoppingItem].self, from: itemsData)
+            
+            self.items = itemsArray
+            
+        } catch {
+            print("Error decoding items: \(error)")
+            
+        }
+    }
+}
