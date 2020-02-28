2e344e12418fbce05e590b1bf2e194b7956743c7
Shawn Gee <shawngee@gmail.com>
2020-02-28 12:55:06 -0500

    Everything working, need to add section headers

--- Shopping List/ViewControllers/ShoppingListCollectionViewController.swift
+++ Shopping List/ViewControllers/ShoppingListCollectionViewController.swift
@@ -71,8 +71,9 @@ class ShoppingListCollectionViewController: UICollectionViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if let submitOrderVC = segue.destination as? SubmitOrderViewController {
+            submitOrderVC.shoppingList = shoppingList
+        }
     }
 
 }
--- Shopping List/ViewControllers/SubmitOrderViewController.swift
+++ Shopping List/ViewControllers/SubmitOrderViewController.swift
@@ -9,22 +9,87 @@
 import UIKit
 
 class SubmitOrderViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var sendOrderButton: UIButton!
+    
+    
+    //MARK: - IBActions
+    
+    @IBAction func sendOrderTapped(_ sender: UIButton) {
+        registerLocalNotification()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Properties
+    
+    var shoppingList: ShoppingList?
+    
+    
+    //MARK: - Private
+    
+    private func registerLocalNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if granted {
+                DispatchQueue.main.async {
+                    self.scheduleLocalNotification()
+                }
+            } else {
+                // Maybe we should show an alert instead if they don't authorize
+                print("User denied authorization to schedule local notifications")
+            }
+        }
     }
-    */
-
+    
+    private func scheduleLocalNotification() {
+        let center = UNUserNotificationCenter.current()
+        guard let name = nameTextField.text,
+            let address = addressTextField.text else { return}
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Delivery for \(name)"
+        content.body = "Your shopping items will be delivered to \(address) in 15 minutes!"
+        content.sound = .default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request) { error in
+            // Should we still show an alert here after scheduling a notification?
+        }
+        
+        // Let user know order was placed by popping back to list
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func updateStatus() {
+        guard let shoppingList = shoppingList else { return }
+        statusLabel.text = "You currently have \(shoppingList.shoppingItemsOnList.count) items in your shopping list."
+    }
+    
+    
+    //MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.delegate = self
+        addressTextField.delegate = self
+        updateStatus()
+    }
 }
+
+extension SubmitOrderViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let nameText = nameTextField.text,
+            let addressText = addressTextField.text else { return }
+
+        sendOrderButton.isEnabled = !nameText.isEmpty && !addressText.isEmpty
+    }
+}
+
