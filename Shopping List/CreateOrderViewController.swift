e15220d2693c35a7fcb9a738dd70d341332b348e
Wyatt Harrell <wyattaharrell@gmail.com>
2020-02-28 13:12:33 -0500

    first stretch goal complete

--- Shopping List/CreateOrderViewController.swift
+++ Shopping List/CreateOrderViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class CreateOrderViewController: UIViewController {
 
@@ -30,6 +31,20 @@ class CreateOrderViewController: UIViewController {
     @IBAction func sendOrderButtonTapped(_ sender: Any) {
         
         guard let name = nameTextField.text, !name.isEmpty, let address = addressTextField.text, !address.isEmpty else { return }
+
+        alert(name: name, address: address)
+    }
+    
+    @IBAction func sendOrderNotification(_ sender: Any) {
+        
+        guard let name = nameTextField.text, !name.isEmpty, let address = addressTextField.text, !address.isEmpty else { return }
+
+        registerLocal()
+        scheduleLocal(name: name, address: address)
+    }
+    
+    
+    func alert(name: String, address: String) {
         
         let alert = UIAlertController(title: "Ordered", message: "Thank you for your order, \(name). It will be delivered to \(address) in 15 minutes.", preferredStyle: .alert)
         
@@ -37,4 +52,40 @@ class CreateOrderViewController: UIViewController {
         alert.addAction(action)
         present(alert, animated: true)
     }
+    
+    
+    func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("granded")
+            } else {
+                print("Not granted")
+            }
+        }
+    }
+    
+    func scheduleLocal(name: String, address: String) {
+
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Ordered"
+        content.body = "Thank you for your order, \(name). It will be delivered to \(address) in 15 minutes."
+        content.categoryIdentifier = "Alarm"
+        content.sound = .default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+
+    }
+    
+    
 }
+
+
+
+
+
