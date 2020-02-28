44b6e014ee13c0b6a0d0de7751a95d7e78eaf451
Harmony Radley <harmonyradley99@gmail.com>
2020-02-28 11:44:01 -0500

    Building UI and created two swift files

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,28 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Harmony Radley on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem: Codable, Equatable {
+    
+    var name: String
+    var hasBeenAdded: Bool
+    var image: UIImage {
+        return UIImage(named: name)!
+    }
+    
+    init(name: String, hasBeenAdded: Bool = false) {
+        self.name = name
+        self.hasBeenAdded = hasBeenAdded
+    }
+    
+    
+}
+
+
+
---
+++ Shopping List/ShoppingListController.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Harmony Radley on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    var itemNames: [ShoppingItem] = [
+        ShoppingItem(name: "Apple"),
+        ShoppingItem(name: "Grapes"),
+        ShoppingItem(name: "Milk"),
+        ShoppingItem(name: "Muffin"),
+        ShoppingItem(name: "Popcorn"),
+        ShoppingItem(name: "Soda"),
+        ShoppingItem(name: "Strawberries"),
+    ]
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+
