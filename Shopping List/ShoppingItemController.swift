9f3d22a4be417cecbbfe0833c221806b2f13cbc8
Wyatt Harrell <wyattaharrell@gmail.com>
2020-02-28 12:50:03 -0500

    MVP

--- Shopping List/ShoppingItemController.swift
+++ Shopping List/ShoppingItemController.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+extension String {
+    static let shouldLoadOrSave = "shouldLoadOrSave"
+}
 
 
 class ShoppingItemController {
@@ -20,23 +23,19 @@ class ShoppingItemController {
     
     
     init() {
-        for item in itemNames {
-            items.append(ShoppingItem(name: item))
+        
+        if !UserDefaults.standard.bool(forKey: .shouldLoadOrSave) {
+            for item in itemNames {
+                items.append(ShoppingItem(name: item))
+            }
+            saveToPersistentStore()
+            print("---------------SAVE---------------")
+            UserDefaults.standard.set(true, forKey: .shouldLoadOrSave)
+        } else if UserDefaults.standard.bool(forKey: .shouldLoadOrSave) {
+            loadFromPersistentStore()
+            print("---------------LOAD---------------")
         }
-        #warning("check user defualts and save")
-
     }
-//    {
-//        var result: [ShoppingItem] = []
-//
-//        for item in itemNames {
-//            result.append(ShoppingItem(name: item))
-//        }
-//
-//        #warning("check user defualts and save")
-//
-//        return result
-//    }
     
     func updateHasBeenAdded(for item: ShoppingItem) {
         
@@ -61,4 +60,43 @@ class ShoppingItemController {
         print(shoppingList)
     }
     
+    
+    // MARK: - Persistence
+    
+    var shoppingItemsURL: URL? {
+        let fileManager = FileManager()
+        
+        let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        return documentsDir?.appendingPathComponent("ShoppingItems.plist")
+    }
+    
+    func saveToPersistentStore() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            
+            let shoppingItemsData = try encoder.encode(items)
+            
+            guard let itemsURL = shoppingItemsURL else { return }
+            
+            try shoppingItemsData.write(to: itemsURL)
+        } catch {
+            print("unable to save items to plist \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        guard let itemsURL = shoppingItemsURL else { return }
+        let decoder = PropertyListDecoder()
+        
+        do {
+            let itemsData = try Data(contentsOf: itemsURL)
+            let decodedItems = try decoder.decode([ShoppingItem].self, from: itemsData)
+            
+            self.items = decodedItems
+        } catch {
+            print("Error decoding items\(error)")
+        }
+    }
 }
