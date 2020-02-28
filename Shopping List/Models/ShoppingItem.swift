0d5644ebf8b0fd084dbb850359da23cdc8202bc4
Hector L <hectoremil21@hotmail.com>
2020-02-28 11:38:31 -0500

    Create ShoppingItem Model

---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Karen Rodriguez on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    let name: String
+    var isAdded: Bool
+    let imageData: Data
+}
