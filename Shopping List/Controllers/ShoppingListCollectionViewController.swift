379f42c9037afba5c74cf77e02b666578dd307d6
Hector L <hectoremil21@hotmail.com>
2020-02-28 16:03:28 -0500

    Code cleanup.

--- Shopping List/Controllers/ShoppingListCollectionViewController.swift
+++ Shopping List/Controllers/ShoppingListCollectionViewController.swift
@@ -20,9 +20,6 @@ class ShoppingListCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         itemController.initValues
         itemController.loadFromPersistentStore()
-        
-        print(itemController.items.count)
-
     }
 
     // MARK: - Navigation
@@ -32,7 +29,6 @@ class ShoppingListCollectionViewController: UICollectionViewController {
         if segue.identifier == "SendOrderSegue" {
             guard let orderVC = segue.destination as? OrderViewController else { return}
             orderVC.itemController = self.itemController
-            print("Segue hit")
             
         }
         
@@ -59,7 +55,7 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ItemCollectionViewCell else {fatalError()}
         
-        var item = itemFor(indexPath: indexPath)
+        let item = itemFor(indexPath: indexPath)
         
         cell.item = item
     
@@ -84,43 +80,9 @@ class ShoppingListCollectionViewController: UICollectionViewController {
             itemController.items[itemIndex].isAdded.toggle()
         }
         
-//        itemController.items[indexPath.row].isAdded.toggle()
         itemController.saveToPersistentStore()
         self.collectionView?.reloadData()
     }
-    
-    
-    
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
-    
     
     func itemFor(indexPath: IndexPath) -> ShoppingItem {
         if indexPath.section == 0 {
