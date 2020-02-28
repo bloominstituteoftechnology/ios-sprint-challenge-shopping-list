4ad12651ad1619e09e95fdd0c476e49e0241c435
Mark Gerrior <github@gerrior.com>
2020-02-28 10:02:08 -0800

    Added Cell class. Organization

---
+++ Shopping List/View Controllers/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Mark Gerrior on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
