0d459873696ec103eeef96f16db2e8cb87e18bc1
Mark Gerrior <github@gerrior.com>
2020-02-28 10:56:03 -0800

    Toggle Shopping cart item (at least the data model

--- Shopping List/View Controllers/CartCollectionViewController.swift
+++ Shopping List/View Controllers/CartCollectionViewController.swift
@@ -110,12 +110,13 @@ class CartCollectionViewController: UICollectionViewController, DeliveryAlertDel
     }
     */
 
-    /*
     // Uncomment this method to specify if the specified item should be selected
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        
+        cartController.cart[indexPath.item].onShoppingList.toggle()
+        
         return true
     }
-    */
 
     /*
     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
