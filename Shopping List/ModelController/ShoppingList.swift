d813a19eaaa2988f78a31caf9a6ad4af9b908e8c
Shawn Gee <shawngee@gmail.com>
2020-02-28 11:27:52 -0500

    Model and model controller added

---
+++ Shopping List/ModelController/ShoppingList.swift
@@ -0,0 +1,75 @@
+//
+//  ShoppingList.swift
+//  Shopping List
+//
+//  Created by Shawn Gee on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+
+class ShoppingList {
+    
+    private(set) var shoppingItems: [ShoppingItem]
+    
+    
+    
+    
+    //MARK: - CRUD
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - Init
+    
+    init() {
+        shoppingItems = [ShoppingItem]()
+        
+        if UserDefaults.standard.bool(forKey: "shoppingItemsInitialized") {
+            loadFromPersistentStore()
+        } else {
+            let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+            itemNames.forEach { shoppingItems.append(ShoppingItem(name: $0, isOnShoppingList: false))}
+            saveToPersistentStore()
+            UserDefaults.standard.set(true, forKey: "shoppingItemsInitialized")
+        }
+    }
+    
+    
+    //MARK: - Persistence
+    
+    private let fileName = "ShoppingList.plist"
+    
+    private var shoppingListURL: URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        return documentsDirectory.appendingPathComponent(fileName)
+    }
+    
+    private func saveToPersistentStore() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let shoppingList = try encoder.encode(shoppingItems)
+            try shoppingList.write(to: shoppingListURL)
+        } catch {
+            print("Couldn't save shopping list to persistent store, error: \(error)")
+        }
+    }
+    
+    private func loadFromPersistentStore() {
+        let decoder = PropertyListDecoder()
+        
+        do {
+            let shoppingListData = try Data(contentsOf: shoppingListURL)
+            shoppingItems = try decoder.decode([ShoppingItem].self, from: shoppingListData)
+        } catch {
+            print("Couldn't load shopping list from persistent store, error: \(error)")
+        }
+    }
+}
