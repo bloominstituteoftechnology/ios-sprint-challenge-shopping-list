1a8d409199ec2ee5f73d6227232bbacbe4cdfdb6
Mark Gerrior <github@gerrior.com>
2020-02-28 09:03:48 -0800

    Checkout initial wireup

--- Shopping List/CheckoutViewController.swift
+++ Shopping List/CheckoutViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 class CheckoutViewController: UIViewController {
 
+    var cartItemCount = 2
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBAction func send(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var descriptionText = ""
+        
+        switch cartItemCount {
+        case 0:
+            descriptionText = "Your shopping list is empty."
+        case 1:
+            descriptionText = "You currently have \(cartItemCount) item in your shopping list."
+        default:
+            descriptionText = "You currently have \(cartItemCount) items in your shopping list."
+        }
+        
+        descriptionLabel.text = descriptionText
 
         // Do any additional setup after loading the view.
     }
