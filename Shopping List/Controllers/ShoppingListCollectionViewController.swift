8f9509d1fd2615b36a49582fec19cae51f39631e
Hector L <hectoremil21@hotmail.com>
2020-02-28 13:10:25 -0500

    Set up prepare for segue

--- Shopping List/Controllers/ShoppingListCollectionViewController.swift
+++ Shopping List/Controllers/ShoppingListCollectionViewController.swift
@@ -25,15 +25,17 @@ class ShoppingListCollectionViewController: UICollectionViewController {
 
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "SendOrderSegue" {
+            guard let orderVC = segue.destination as? OrderViewController else { return}
+            orderVC.itemController = self.itemController
+            
+        }
+        
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
