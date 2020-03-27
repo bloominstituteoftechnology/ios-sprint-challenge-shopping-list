ee74033d39fd15c7259f34cee9c1dafc6df2f3c2
Joe VeVerka <joeveverka89@gmail.com>
2020-03-27 11:27:34 -0400

    Working on storyBoard

---
+++ Shopping List/Model /ShoppingListModel.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingListModel.swift
+//  Shopping List
+//
+//  Created by Marissa Gonzales on 3/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    
+    var name: String
+    var hasBeenAdded: Bool
+    
+//    var itemPics
+    
+    
+}
