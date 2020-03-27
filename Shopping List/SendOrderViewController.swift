8389e92b3b63d452ecbc117d892522ab7e8d748a
Chris Dobek <dobek.christopher@gmail.com>
2020-03-27 11:27:27 -0400

    Starting Shopping List Project

---
+++ Shopping List/SendOrderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SendOrderViewController.swift
+//  Shopping List
+//
+//  Created by Chris Dobek on 3/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SendOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shopping List/ShoppingList.swift
@@ -0,0 +1,10 @@
+//
+//  ShoppingList.swift
+//  Shopping List
+//
+//  Created by Chris Dobek on 3/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
