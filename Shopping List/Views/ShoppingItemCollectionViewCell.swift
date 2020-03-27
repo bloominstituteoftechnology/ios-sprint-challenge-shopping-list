f35b23270afd4bbf25ef9775de7cc74c22a81953
Cody Morley <codymorleyinc@gmail.com>
2020-03-27 09:43:01 -0700

    finished creating custom classes

---
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Bling Morley on 3/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
