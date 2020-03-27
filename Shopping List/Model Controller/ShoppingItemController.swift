2b8190f0b8cb2f369b6f7ca3b8ae8aa5b8af7693
Nihal Erdal <nihalerdall@gmail.com>
2020-03-27 08:37:16 -0700

    created storyboard and files

---
+++ Shopping List/Model Controller/ShoppingItemController.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Nihal Erdal on 3/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Shopping List/View/ShoppingListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Nihal Erdal on 3/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewCell: UICollectionViewCell {
+    
+}
