1f8669bd8034d859d7ec6eb3e5182ae918b8d126
Shawn-James <shawn.james@me.com>
2020-03-27 12:17:56 -0400

    add collection view controller

--- Shopping List/ViewControllers/ShoppingListCollectionViewController.swift
+++ Shopping List/ViewControllers/ShoppingListCollectionViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ShoppingListCollectionViewController: UICollectionViewController {
 
+    // MARK: - Model Controller Reference Property
+    
+    let shoppingItemController = ShoppingItemController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,60 +23,36 @@ class ShoppingListCollectionViewController: UICollectionViewController {
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == String.segueIdentifier {
+        if segue.identifier == .segueIdentifier {
             // Get the new view controller using [segue destinationViewController].
-            guard let shoppingCartViewController = segue.destination as? ShoppingCartViewController,
-                let indexPath = <#code#> else { fatalError("failure to downcast segue destination")}
+            guard let shoppingCartViewController = segue.destination as? ShoppingCartViewController else { fatalError("failure to downcast segue destination")}
             // Pass the selected object to the new view controller.
+            // FIXME: - pass shopping shoppingitemcontroller to destination's shopping list
         }
     }
 
     // MARK: UICollectionViewDataSource
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
-    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { shoppingItemController.shoppingItems.count }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: .reuseIdentifier, for: indexPath)
-    
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: .reuseIdentifier, for: indexPath) as? ShoppingListCollectionViewCell else { fatalError("failure to dequeue cell") }
         // Configure the cell
-    
+        // FIXME: - add configuration
         return cell
     }
 
     // MARK: UICollectionViewDelegate
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        var item = shoppingItemController.shoppingItems[indexPath.item]
+        item.isAdded.toggle()
+        shoppingItemController.shoppingItems[indexPath.item] = item
+        shoppingItemController.saveToPersistentStore()
+        collectionView.reloadData()
         return true
     }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
-
 }
 
 extension String {
