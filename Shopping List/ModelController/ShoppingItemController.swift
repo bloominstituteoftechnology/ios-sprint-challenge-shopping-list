1d4ebe7404302008167d66ddaf58156fdff723b4
violet-love <violetlove9419@icloud.com>
2020-04-24 11:00:38 -0700

    set up an array of grocery items

---
+++ Shopping List/ModelController/ShoppingItemController.swift
@@ -0,0 +1,35 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Violet Lavender Love on 4/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+
+class ShoppingItemController {
+    
+    let itemNames: [ShoppingItem] = [ShoppingItem(name: "Apple",
+                                                  isOnShoppingList: false, image: "apple"),
+                                     ShoppingItem(name: "Grapes",
+                                                  isOnShoppingList: false, image: "grapes"),
+                                     ShoppingItem(name:"Milk", isOnShoppingList:
+                                        false, image: "milk"),
+                                     ShoppingItem(name: "Muffin",
+                                                  isOnShoppingList: false, image: "muffin"),
+                                     ShoppingItem(name: "Popcorn",
+                                                  isOnShoppingList: false, image: "popcorn"),
+                                     ShoppingItem(name: "Soda", isOnShoppingList:
+                                        false, image: "soda"),
+                                     ShoppingItem(name: "Strawberries",
+                                                  isOnShoppingList: false, image: "strawberries")]
+    
+    
+    
+    
+    
+    
+}
