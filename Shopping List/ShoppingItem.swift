e590ae86c8d5acd78fe2f0f0e5a17562ea485940
violet-love <violetlove9419@icloud.com>
2020-04-24 09:38:22 -0700

    kinda got the grocery  item struct set up and the collection view cell. I'm tryin

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Violet Lavender Love on 4/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+
+struct ShoppingItem {
+    
+    let name: String
+    let isOnShoppingList: Bool
+    let image: UIImage
+    init(name: String, isOnShoppingList: Bool, image: String) {
+        
+        self.name = name
+        self.isOnShoppingList = isOnShoppingList
+        self.image = UIImage(named: imageName)!
+        
+    }
+}
