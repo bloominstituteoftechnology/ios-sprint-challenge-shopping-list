7dc30888c2b77c9aa89990a24912b7261bca3bd0
Ronald Huston Jr <rghuston9@gmail.com>
2020-04-24 18:19:32 -0500

    CheckoutViewController

--- Shopping List/view controllers/CheckoutViewController.swift
+++ Shopping List/view controllers/CheckoutViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class CheckoutViewController: UIViewController {
-
+    @IBOutlet weak var checkoutMsg: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    var itemsCount: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        guard let itemCount = itemsCount else { return }
+        checkoutMsg.text = "you currently have \(itemCount) items in your shopping list."
     }
 
+    @IBAction func sendOrder(_ sender: Any) {
+        let alert = UIAlertController(title: "special delivery for \(nameTextField.text ?? "sorry - not found")" , message: "your shopping list will be delivered to \(addressTextField.text ?? "no such number").", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
 }
