2be0a2a42f5a840159a93607005babbfbe169239
Clayton Watkins <watkinclayton17@yahoo.com>
2020-05-01 18:27:12 -0700

    Created modelController added persistence and user defualt

--- Shopping List/Model/Model Controller/ShoppingController.swift
+++ Shopping List/Model/Model Controller/ShoppingController.swift
@@ -13,7 +13,56 @@ class ShoppingControler {
     //MARK: - Properties
     var shoppingItems: [ShoppingItem] = []
     
-
+    private let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
     
+ init(){
+    let isInitiated = UserDefaults.standard.bool(forKey: .isInitialized)
+        if isInitiated {
+            loadFromPersistentStore()
+        } else {
+            createShoppingItem()
+        }
+    }
+    
+    //MARK: - CRUD
+    //Create:
+    func createShoppingItem(){
+        for name in itemNames{
+            let newItem = ShoppingItem(itemName: name, imageName: name)
+            shoppingItems.append(newItem)
+            saveToPersistentStore()
+        }
+    }
+    
+    //MARK: - Persistence
+    var persistentFileURL: URL?{
+        let fm = FileManager.default
+        let documents = fm.urls(for: .documentDirectory, in: .userDomainMask).first
+        return documents?.appendingPathComponent("ShoppingItems.plist")
+    }
+    
+    func saveToPersistentStore(){
+        guard let url = persistentFileURL else { return }
+        do{
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(self.shoppingItems)
+            try data.write(to: url)
+        }catch{
+            print("Error encoding items to ShoppingItems.plist")
+        }
+    }
+    
+    func loadFromPersistentStore(){
+        let fm = FileManager.default
+        guard let url = persistentFileURL,
+            fm.fileExists(atPath: url.path) else { return }
+        do{
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            self.shoppingItems = try decoder.decode([ShoppingItem].self, from: data)
+        }catch{
+            print("Error decoding items from ShoppingItem.plist")
+        }
+    }
     
 }
