093979f01b625f7f16011a19f5249e742817cb69
bohdan462 <tech.bohdan@gmail.com>
2020-05-02 11:59:11 -0500

    Finished setup StoryBoard and model

---
+++ Shopping List/Model Controller/ShoppingListController.swift
@@ -0,0 +1,12 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Bohdan Tkachenko on 5/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+
---
+++ Shopping List/Views/ShoppingListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Bohdan Tkachenko on 5/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewCell: UICollectionViewCell {
+    
+}
