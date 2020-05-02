bb85f8ab8b1e6e5b12fd31acd9219c886fae016f
bohdan462 <tech.bohdan@gmail.com>
2020-05-02 13:06:47 -0500

    updateViews added

--- Shopping List/Views/ShoppingListCollectionViewCell.swift
+++ Shopping List/Views/ShoppingListCollectionViewCell.swift
@@ -14,6 +14,25 @@ class ShoppingListCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var itemNameLabel: UILabel!
     
+    var shoppinglistController: ShoppingListController?
     
+    var shoppingList: ShoppingItem? {
+        didSet{
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let shoppingList = shoppingList else { return }
+        
+        itemImageView.image = UIImage(named: shoppingList.name)
+        itemNameLabel.text = shoppingList.name
+        
+        if shoppingList.hasBeenAdded == false {
+            addItemLabel.text = "Not Added"
+        } else {
+            addItemLabel.text = "Added"
+        }
+    }
     
 }
