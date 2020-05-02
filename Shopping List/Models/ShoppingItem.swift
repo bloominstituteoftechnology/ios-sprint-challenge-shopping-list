66df8755ea69f4d4c55cd1214d6f4542c5f012fa
Hannah <hannahlbain89@gmail.com>
2020-05-02 12:11:54 -0500

    Set up groups and initial commit

---
+++ Shopping List/Models/ShoppingItem.swift
@@ -0,0 +1,22 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Hannah Bain on 5/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    let apple: Bool
+    let grapes: Bool
+    let milk: Bool
+    let muffin: Bool
+    let popcorn: Bool
+    let soda: Bool
+    let strawberries: Bool 
+}
+
+
+
