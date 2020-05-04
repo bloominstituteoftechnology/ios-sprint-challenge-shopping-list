ad6d7d23a0bb0ac4e97362f6867ff729f8b7a1c8
Aaron Peterson <aarondp2011@gmail.com>
2020-05-03 20:46:30 -0500

    Set up model

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Aaron Peterson on 5/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct shoppingItem {
+    
+    var name: String
+    var isAdded: Bool = false
+    var image: UIImage
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.image = UIImage(named: imageName)!
+    }
+}
