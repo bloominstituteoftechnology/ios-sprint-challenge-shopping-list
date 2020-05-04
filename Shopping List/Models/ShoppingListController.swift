cd55884aea2b1535b326269efdfda003974dee46
Ian French <igf2011@hotmail.com>
2020-05-03 22:02:49 -0400

    Second commit. Storyboard layout complete.

--- Shopping List/Models/ShoppingListController.swift
+++ Shopping List/Models/ShoppingListController.swift
@@ -7,3 +7,92 @@
 //
 
 import Foundation
+
+
+class ShoppingController {
+
+ let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+
+var shopList: [ShopList] = []
+
+var foods: [ShopList] {
+    
+    return shopList.filter{$0.itemAdd == true}
+    
+
+}
+   
+     var shopListURL: URL? {
+                 let fm = FileManager.default
+             
+                 guard let directory = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+             
+              
+              let shopListURL = directory.appendingPathComponent("ShoppingList.plist")
+              return shopListURL
+             }
+    
+    func saveToPersistentStore() {
+                
+                  do {
+                    guard let url = shopListURL else { return }
+
+                    let encoder = PropertyListEncoder()
+                    let shopData = try encoder.encode(shopList)
+                    try shopData.write(to: url)
+                } catch {
+                    NSLog("Error saving books data: \(error)")
+                }
+            }
+    
+    
+    
+    func loadFromPersistentStore() {
+                 
+                   do {
+                      let fm = FileManager.default
+                      guard let url = shopListURL,
+                      fm.fileExists(atPath: url.path) else { return }
+                 
+               
+                     let data = try Data(contentsOf: url)
+                     let decoder = PropertyListDecoder()
+                     let decodedItems = try decoder.decode([ShopList].self, from: data)
+                     shopList = decodedItems
+                 } catch {
+                     NSLog("Error loading books data: \(error)")
+                 }
+             }
+    
+    
+    
+    
+    
+      
+    func addItem(item: ShopList, addItem: Bool){
+        guard let itemIndex = foods.firstIndex(of: item) else
+        { return }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
