8e5b50233711d1d58dba0ece87df72b4608ef2c9
violet-love <violetlove9419@icloud.com>
2020-05-22 09:54:45 -0700

    added array of Shopping Items, started on a settings key

---
+++ Shopping List/Helpers/SettingsKey.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsKey.swift
+//  Shopping List
+//
+//  Created by Violet Lavender Love on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
--- Shopping List/ModelController/ShoppingItemController.swift
+++ Shopping List/ModelController/ShoppingItemController.swift
@@ -10,7 +10,16 @@ import Foundation
 
 class ShoppingListController {
     
-    
+    var shoppingListItems: [ShoppingItem] {
+        [ShoppingItem(name: "Apple", hasBeenAdded: false, imageName: "Apple"),
+        ShoppingItem(name: "Grapes", hasBeenAdded: false, imageName: "Grapes"),
+        ShoppingItem(name: "Milk", hasBeenAdded: false, imageName: "Milk"),
+        ShoppingItem(name: "Muffin", hasBeenAdded: false, imageName: "Muffin"),
+        ShoppingItem(name: "Popcorn", hasBeenAdded: false, imageName: "Popcorn"),
+        ShoppingItem(name: "Soda", hasBeenAdded: false, imageName: "Soda"),
+        ShoppingItem(name: "Strawberries", hasBeenAdded: false, imageName: "Strawberries")
+    ]
+    }
     
     
     
