d9ad5b256169feb68f8ef6d52e69b740f72c404a
violet-love <violetlove9419@icloud.com>
2020-05-22 10:32:04 -0700

    set up userdefaults?

--- Shopping List/Helpers/SettingsKey.swift
+++ Shopping List/Helpers/SettingsKey.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+extension String {
+    
+    static var hasBeenAddedKey = "HasBeenAdded"
+}
--- Shopping List/ModelController/ShoppingItemController.swift
+++ Shopping List/ModelController/ShoppingItemController.swift
@@ -11,15 +11,24 @@ import Foundation
 class ShoppingListController {
     
     var shoppingListItems: [ShoppingItem] {
+        var shoppingListItems =
         [ShoppingItem(name: "Apple", hasBeenAdded: false, imageName: "Apple"),
-        ShoppingItem(name: "Grapes", hasBeenAdded: false, imageName: "Grapes"),
-        ShoppingItem(name: "Milk", hasBeenAdded: false, imageName: "Milk"),
-        ShoppingItem(name: "Muffin", hasBeenAdded: false, imageName: "Muffin"),
-        ShoppingItem(name: "Popcorn", hasBeenAdded: false, imageName: "Popcorn"),
-        ShoppingItem(name: "Soda", hasBeenAdded: false, imageName: "Soda"),
-        ShoppingItem(name: "Strawberries", hasBeenAdded: false, imageName: "Strawberries")
-    ]
+         ShoppingItem(name: "Grapes", hasBeenAdded: false, imageName: "Grapes"),
+         ShoppingItem(name: "Milk", hasBeenAdded: false, imageName: "Milk"),
+         ShoppingItem(name: "Muffin", hasBeenAdded: false, imageName: "Muffin"),
+         ShoppingItem(name: "Popcorn", hasBeenAdded: false, imageName: "Popcorn"),
+         ShoppingItem(name: "Soda", hasBeenAdded: false, imageName: "Soda"),
+         ShoppingItem(name: "Strawberries", hasBeenAdded: false, imageName: "Strawberries")
+        ]
+        let hasBeenAdded = UserDefaults.standard.bool(forKey: .hasBeenAddedKey)
+        
+        if hasBeenAdded {
+            //change button to show that, something i haven't had time to teach myself because i'm exhausted 
+        }
+        
+        return shoppingListItems
     }
+    
     
     
     
