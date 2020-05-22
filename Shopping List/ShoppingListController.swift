dc6f25217f6ec163deaa8e7278a46cc95d08f8e0
Ian Becker <ian-becker@lambdastudents.com>
2020-05-22 09:44:27 -0700

    Created corresponding models and controllers, working on persistence.

---
+++ Shopping List/ShoppingListController.swift
@@ -0,0 +1,22 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Ian Becker on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListController {
+    
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+    
+    func bool(forKey defaultName: String) -> Bool {
+        
+    }
+    func set(_ value: Bool, forKey defaultName: String) {
+        
+    }
+    
+}
