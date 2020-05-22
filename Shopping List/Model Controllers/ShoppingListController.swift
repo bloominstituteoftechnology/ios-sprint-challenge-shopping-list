6c9c8ff8cb9c43e620f39003f481555e6096695a
Ian Becker <ian-becker@lambdastudents.com>
2020-05-22 10:40:10 -0700

    Trying to figure out how to get the list of items to show up every time upon loading the app, still need to change from not added to added, and give an alert message.

---
+++ Shopping List/Model Controllers/ShoppingListController.swift
@@ -0,0 +1,25 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by Ian Becker on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListController {
+    
+    var groceryItems: [ShoppingItem] {
+    
+        var itemNames = [ShoppingItem(groceryItem: "Apple"),
+                         ShoppingItem(groceryItem: "Grapes"),
+                         ShoppingItem(groceryItem: "Milk"),
+                         ShoppingItem(groceryItem: "Muffin"),
+                         ShoppingItem(groceryItem: "Popcorn"),
+                         ShoppingItem(groceryItem: "Soda"),
+                         ShoppingItem(groceryItem:"Strawberries")]
+        
+        
+    
+}
