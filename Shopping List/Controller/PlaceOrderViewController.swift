bd1f3c0e03b7a0e17ee7f43a3ea396e3af3eb585
wrscomputers <wrscomputers@gmail.com>
2020-05-22 13:51:04 -0400

    Added ShoppingListColletionViewCell and linked to storyboard, finished ShoppingListCollectionViewController

--- Shopping List/Controller/PlaceOrderViewController.swift
+++ Shopping List/Controller/PlaceOrderViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PlaceOrderViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Shopping List/Controller/ShoppingListCollectionViewController.swift
+++ Shopping List/Controller/ShoppingListCollectionViewController.swift
@@ -8,49 +8,40 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class ShoppingListCollectionViewController: UICollectionViewController {
 
+    let shoppingItemsController = ShoppingItemController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
     }
 
-    /*
+   
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toOrderScreen" {
+            guard let placeOrderVC = segue.destination as? PlaceOrderViewController else {
+                return
+            }
+            
+            
+            
+        }
     }
-    */
+
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return shoppingItemsController.itemsAvailable.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShoppingItems", for: indexPath) as? ShoppingListCollectionViewCell else { fatalError("Collection view cell identifier is wrong or the cell is not a ShoppingListCollectionViewCell")}
     
-        // Configure the cell
     
         return cell
     }
