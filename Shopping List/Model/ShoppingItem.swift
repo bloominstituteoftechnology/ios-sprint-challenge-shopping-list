c62c53b734c79ca718619ab0a63e417d08a1bfc4
violet-love <violetlove9419@icloud.com>
2020-05-22 09:30:42 -0700

    created grocery list item

---
+++ Shopping List/Model/ShoppingItem.swift
@@ -0,0 +1,25 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Violet Lavender Love on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct ShoppingItem {
+    
+    let name: String
+    var hasBeenAdded: Bool
+    var image: UIImage
+    
+    init(name: String, hasBeenAdded: Bool, imageName: String) {
+        self.name = name
+        self.hasBeenAdded = hasBeenAdded
+        self.image = UIImage(named: imageName)!
+    }
+    
+    
+    
+}
