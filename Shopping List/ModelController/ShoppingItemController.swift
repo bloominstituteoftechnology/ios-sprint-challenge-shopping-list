97c2effb73f62bf285a53a9286de2aa058b11964
violet-love <violetlove9419@icloud.com>
2020-05-22 09:40:43 -0700

    started Shopping Item Class

---
+++ Shopping List/ModelController/ShoppingItemController.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Violet Lavender Love on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    
+    
+    
+    
+}
