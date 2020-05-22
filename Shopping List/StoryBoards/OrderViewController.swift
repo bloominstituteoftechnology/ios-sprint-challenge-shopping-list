7c9297750e7089705cb247f7e37b9b33a819f3cd
violet-love <violetlove9419@icloud.com>
2020-05-22 15:53:52 -0700

    wired up some outlets for the order view controller

---
+++ Shopping List/StoryBoards/OrderViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OrderViewController.swift
+//  Shopping List
+//
+//  Created by Violet Lavender Love on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    @IBOutlet var itemAmountLabel: UILabel!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var addressTextField: UITextField!
+    @IBOutlet var sendOrderButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
