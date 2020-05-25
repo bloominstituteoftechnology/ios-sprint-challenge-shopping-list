3e19307cf98572b8550ba6c4c19798ea6f6875fc
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-25 10:44:14 -0700

    xcode troubles

---
+++ Shopping List/VIews/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Richard Gibbs on 5/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+    
+}
