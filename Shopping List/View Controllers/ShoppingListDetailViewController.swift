38dcb778d5fd85af0f31fea45fc8299ab40954f3
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-25 23:28:10 -0700

    everything works I believe I just need to add persistence now

--- Shopping List/View Controllers/ShoppingListDetailViewController.swift
+++ Shopping List/View Controllers/ShoppingListDetailViewController.swift
@@ -17,54 +17,58 @@ class ShoppingListDetailViewController: UIViewController {
             numberOfItemsLabel.text = "You currently have \(itemsInCart) item(s) in your shopping list"
         }
     }
-
-
-@IBOutlet weak var numberOfItemsLabel: UILabel!
-@IBOutlet weak var userAddressTextField: UITextField!
-@IBOutlet weak var userNameTextField: UITextField!
-@IBOutlet weak var submitOrderButton: UIButton!
-
-@IBAction func tappedSubmitOrder(_ sender: Any) {
-    showAlert()
-}
-
+    
+    
+    @IBOutlet weak var numberOfItemsLabel: UILabel!
+    @IBOutlet weak var userAddressTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var submitOrderButton: UIButton!
+    
+    @IBAction func tappedSubmitOrder(_ sender: Any) {
+        showAlert()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
     }
     
-func showAlert() {
-    let alert = UIAlertController(title: "Order Successful!!!", message: "\(userNameTextField.text ?? " ") your order will be delivered to \(userAddressTextField.text ?? " ")in 15 mins", preferredStyle: .alert)
+    func showAlert() {
+        let alert = UIAlertController(title: "Order Successful!!!", message: "\(userNameTextField.text ?? " ") your order will be delivered to \(userAddressTextField.text ?? " ")in 15 mins", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        present(alert,animated: true, completion: nil)
+    }
     
-    let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-    alert.addAction(okAction)
-    present(alert,animated: true, completion: nil)
-}
-
-override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-//    guard let shoppingItemController = shoppingItemController else { return }
-//    itemsInCart = shoppingItemController.shoppingItems.count
-//    numberOfItemsLabel.text = "You currently have \(itemsInCart) items(s) in your shopping list"
-}
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //    guard let shoppingItemController = shoppingItemController else { return }
+        //    itemsInCart = shoppingItemController.shoppingItems.count
+        //    numberOfItemsLabel.text = "You currently have \(itemsInCart) items(s) in your shopping list"
+    }
     func updateViews() {
         if let shoppingController = shoppingItemController {
-            let cartItems = shoppingController.shoppingItems.filter({$0.added == false})
-            itemsInCart += 1
-            numberOfItemsLabel.text = "You currently have \(itemsInCart) item(s) in your shopping list"
-            print(cartItems)
-            print(itemsInCart)
+            let cartItems = shoppingController.shoppingItems
+            for items in cartItems {
+                if items.added == true{
+                    itemsInCart += 1
+                }
+                numberOfItemsLabel.text = "You currently have \(itemsInCart) item(s) in your shopping list"
+                print(cartItems)
+                print(itemsInCart)
+            }
         }
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
 }
