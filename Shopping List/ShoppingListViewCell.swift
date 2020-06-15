1c57bc5e1277fd10180e09a14bfff1f08f55f8f0
Craig Belinfante <craigbasketball@aol.com>
2020-06-14 20:51:31 -0700

    craigbel sprint3

---
+++ Shopping List/ShoppingListViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingListViewCell.swift
+//  Shopping List
+//
+//  Created by Craig Belinfante on 6/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
